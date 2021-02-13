616c763cd56bb17dee0723fff33ff789fa3188cc
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-12 23:36:14 -0800

    update bedwars formatting

--- status.js
+++ status.js
@@ -88,7 +88,7 @@ async function txtStatus(name) {
         } else if (status.gameType == 'ARCADE') {
             arcadeFormatter(status)
         } else if (status.gameType == 'BEDWARS') {
-            str += `Bedwars ${status.mode.toLowerCase().replace('_',' ')} - ${status.map}`
+            str += `Bedwars ${status.mode.toLowerCase().replace('_',' ').slice(lastIndexOf(" "))} - ${status.map}`
         } else if (status.gameType == 'TNTGAMES') {
             str += `Tnt ${status.mode.toLowerCase()} - ${status.map}`
         } else if (status.gameType == 'BUILD_BATTLE') {
