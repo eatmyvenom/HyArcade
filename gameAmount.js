3d7d3867bf52c9629b2403c8a6bcfde379a75b16
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-18 01:44:27 -0800

    add more amounts

--- gameAmount.js
+++ gameAmount.js
@@ -6,13 +6,16 @@ async function formatCounts() {
     let counts = await getGameCounts();
     counts = counts.games;
 
-    str += `Arcade Total      : ${counts.ARCADE.players}\n`
-    str += `Party Games Total : ${counts.ARCADE.modes.PARTY}\n`
-    str += `HITW Total        : ${counts.ARCADE.modes.HOLE_IN_THE_WALL}\n`
-    str += `Farm Hunt Total   : ${counts.ARCADE.modes.FARM_HUNT}\n`
-    str += `Limbo Total       : ${counts.LIMBO.players}\n`
-    str += `Idle Total        : ${counts.IDLE.players}\n`
-    str += `Queue Total       : ${counts.QUEUE.players}`
+    str += `Arcade Total        : ${counts.ARCADE.players}\n`
+    str += `Party Games Total   : ${counts.ARCADE.modes.PARTY}\n`
+    str += `HITW Total          : ${counts.ARCADE.modes.HOLE_IN_THE_WALL}\n`
+    str += `Farm Hunt Total     : ${counts.ARCADE.modes.FARM_HUNT}\n`
+    str += `Throw out Total     : ${counts.THROW_OUT.players}\n`
+    str += `Hypixel says Total  : ${counts.SIMON_SAYS.players}\n`
+    str += `Ctw Total           : ${counts.PVP_CTW.players}\n`
+    str += `Idle Total          : ${counts.IDLE.players}\n`
+    str += `Queue Total         : ${counts.QUEUE.players}\n`
+    str += `Total               : ${counts.playerCount.players}`
 
     return str;
 }
