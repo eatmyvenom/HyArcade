0ccba0cdbe92179546380233356f712a93564e03
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-23 02:46:49 -0800

    change config format and move username/avatar

--- src/webhook.js
+++ src/webhook.js
@@ -2,12 +2,10 @@ const config = require('../config.json');
 const Discord = require("discord.js");
 
 async function sendToDiscord(content) {
-    let hook = new Discord.WebhookClient(config.hookid, config.hooktoken);
+    let hook = new Discord.WebhookClient(config.webhook.id, config.webhook.token);
     await hook.send("```" + content + "======================```", {
-        // impersonate ugy because he does daily lb 
-        // in official party games discord server
-        username: 'Ujtagrovec',
-        avatarURL: 'https://cdn.discordapp.com/avatars/726113755796340770/51cb1ec93b38916bef239bc9681f17fa.webp'
+        username: config.webhook.username,
+        avatarURL: config.webhook.pfp
     });
     // this closes the hook client so the nodejs doesnt hang
     // forever
