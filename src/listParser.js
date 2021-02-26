de04163e1aa9d9dab3f02e36653fe1d277677565
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-26 01:12:54 -0800

    begin work on moving lists to json

---
+++ src/listParser.js
@@ -0,0 +1,10 @@
+exports.players = function players(acclist) {
+    let Player = require('./player')(acclist);
+    
+    let playerjson = require('../playerlist.json');
+    let playerlist = [];
+    for (let i = 0; i < playerjson.length; i++) {
+        playerlist.push(new Player(playerjson[i].name,playerjson[i].accs,0))
+    }
+    return playerlist;
+}
