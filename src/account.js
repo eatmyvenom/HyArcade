2d974f7bb3bd738664bfcf0bb149d0e3a2223716
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-02 13:09:21 -0800

    track more data

--- src/account.js
+++ src/account.js
@@ -11,6 +11,7 @@ module.exports = class Account {
     hitwQual=0;
     hitwFinal=0;
     farmhuntWins=0;
+    ranksGifted=0;
 
     constructor(name,wins,uuid){
         this.name = name;
@@ -23,18 +24,30 @@ module.exports = class Account {
         // make sure player has stats to be checked
         if(json.player && json.player.stats && json.player.stats.Arcade) {
             let arcade = json.player.stats.Arcade;
+
             let wins = 0;
             if(arcade.wins_party)   wins += arcade.wins_party;
             if(arcade.wins_party_2) wins += arcade.wins_party_2;
             if(arcade.wins_party_3) wins += arcade.wins_party_3;
             this.wins = wins;
-            this.name = json.player.displayname;
+
             this.rank = json.player.newPackageRank;
+            if(json.player.monthlyPackageRank == 'SUPERSTAR') this.rank += "_PLUS";
+            if(json.player.rank) this.rank = json.player.rank;
+            
+            this.name = json.player.displayname;
+
             this.version = json.player.mcVersionRp;
+
             this.mostRecentGameType = json.player.mostRecentGameType;
+
             this.xp = json.player.networkExp;
+
+            this.ranksGifted = (json.player.giftingMeta != undefined) ? json.player.giftingMeta.ranksGiven : 0 ;
+
             this.hitwFinal = arcade.hitw_record_f;
             this.hitwQual = arcade.hitw_record_q;
+
             this.farmhuntWins = arcade.wins_farm_hunt;
         }
     }
