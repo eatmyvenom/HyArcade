3da24cd62d9d3f15d88d4dae919d99e95e9008a0
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-02 20:19:49 -0800

    allow http requests

--- src/webRequest.js
+++ src/webRequest.js
@@ -1,8 +1,12 @@
-const https = require('https');
+const methods = { https : require('https'), http : require('http') }
 
 module.exports = function webRequest(url) {
     return new Promise((resolve,reject) => {
-        https.get(url, (res) => {
+        let method = 'http';
+        if((""+url).startsWith('https')) {
+            method = 'https'
+        }
+        methods[method].get(url, (res) => {
             let reply = '';
             res.on('data',d=>{reply+=d});
             res.on('end',()=>{resolve({data : reply, headers : res.headers, status : res.statusCode})});
