fc0bdfa32590e4bbb45fb7101b47b4ee535214b7
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-04 17:16:33 -0800

    move strictly cli functions to own module

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 const fs = require('fs/promises');
 const gameAmount = require("./src/gameAmount")
 const Webhook = require('./src/webhook');
-const { getAccountWins, getGuildFromPlayer } = require('./src/hypixelApi');
 const utils = require('./src/utils');
 const dataGen = require('./src/dataGeneration');
-const { getUUID } = require('./src/mojangRequest');
+const cli = require('./src/cli');
 const { listNormal, listDiff, stringNormal, stringDaily } = require('./src/listUtils'); 
 const args = process.argv;
-const sleep = utils.sleep;
-const logger = utils.logger;
 const winsSorter = utils.winsSorter;
 
 // So you may be wondering, "why use such a horrible config 
@@ -18,7 +15,6 @@ const winsSorter = utils.winsSorter;
 // means that if I start adding all kinds of modules, this 
 // will become an issue really fast. So this is my way of not
 // bloating this project with node modules and shit. 
-const config = require('./config.json');
 
 let { accounts } = require("./src/acclist");
 
@@ -64,25 +60,8 @@ async function save() {
     await updateAll();
     // write new data to json files to be used later
     await utils.writeJSON("accounts.json", accounts);
-    await utils.writeJSON("players.json", accounts);
-    await utils.writeJSON("guild.json", accounts);
-}
-
-async function logNormal(name) {
-    logger.out(await stringNormal(name));
-}
-
-// wrappers because I abstracted this
-async function logG() {
-    await logNormal("guild");
-}
-
-async function logP() {
-    await logNormal("players");
-}
-
-async function logA() {
-    await logNormal("accounts");
+    await utils.writeJSON("players.json", players);
+    await utils.writeJSON("guild.json", guilds);
 }
 
 async function webhookLog(type = 'players', maxamnt) {
@@ -119,24 +98,6 @@ async function snap(timeType = 'day') {
     await archive('./',timeType);
 }
 
-async function logDaily(name) {
-    logger.out(await stringDaily(name));
-}
-
-//more abstracted methods
-
-async function logGD() {
-    await logDaily("guild");
-}
-
-async function logPD() {
-    await logDaily("players");
-}
-
-async function logAD() {
-    await logDaily("accounts");
-}
-
 async function genStatus() {
     return await dataGen.genStatus();
 }
@@ -147,59 +108,6 @@ async function genStatus() {
 async function gameAmnt() {
     // write to file so that there isnt blank files in website at any point
     await fs.writeFile('games.txt',await gameAmount.formatCounts())
-}
-
-async function newAcc() {
-    let name = args[3];
-    let category = args[4];
-    let uuid = await getUUID(name);
-    let wins = await getAccountWins(uuid);
-    let acclist = require('./acclist.json');
-    if (acclist[category].find(acc=>acc.uuid == uuid)) {
-        logger.err("Refusing to add duplicate!");
-    } else if (wins < 50 && category == 'gamers') {
-        logger.err("Refusing to add account with under 50 wins to gamers!");
-    }else {
-        acclist[category].push({ name : name, wins : wins, uuid: uuid });
-        await utils.writeJSON('./acclist.json',acclist);
-        logger.out(`${name} with ${wins} wins added.`);
-    }
-}
-
-async function newPlayer() {
-    let name = args[3];
-    let alts = args.slice(4);
-
-    // construct object
-    let playerObj = { name : name , accs : alts };
-
-    // add object to list
-    let plrlist = require('./playerlist.json');
-    plrlist.push(playerObj);
-
-    // write new list
-    await utils.writeJSON('./playerlist.json', plrlist);
-    logger.out(`Player "${name}" has been added with ${alts.length} alts.`)
-}
-
-async function newGuild() {
-    let playerUUID = args[3];
-
-    // get data from hypixel
-    let gldInfo = JSON.parse(await getGuildFromPlayer(playerUUID));
-    
-    // create the actual guild object
-    let id = gldInfo.guild._id;
-    let name = gldInfo.guild.name;
-    let gldObj = { id : id, name : name};
-    
-    // add object to list
-    let gldLst = require('./guildlist.json');
-    gldLst.push(gldObj);
-
-    // write new list
-    await utils.writeJSON('./guildlist.json', gldlLst);
-    logger.out(`Guild "${name} has been added successfully.`);
 }
 
 async function archive(path = './archive/', timetype = utils.day()) {
@@ -232,26 +140,33 @@ async function main(){
     // use different functions for different args
     // switch has one x86 instruction vs multiple for if statements
     switch (args[2]) {
-        case 'logG':        await logG();                               break;
-        case 'logA':        await logA();                               break;
-        case 'logP':        await logP();                               break;
-        case 'logGD':       await logGD();                              break;
-        case 'logPD':       await logPD();                              break;
-        case 'logAD':       await logAD();                              break;
+        case 'logG':        await cli.logNormal("guild");               break;
+        case 'logA':        await cli.logNormal("accounts");            break;
+        case 'logP':        await cli.logNormal("players");             break;
+        
+        case 'logGD':       await cli.logDaily("guild");                break;
+        case 'logAD':       await cli.logDaily("accounts");             break;
+        case 'logPD':       await cli.logDaily("players");              break;
 
         case 'write':       await writeFile(args);                      break;
         case 'writeD':      await writeFileD(args);                     break;
 
         case 'save':        await save();                               break;
         case 'snap':        await snap(args[3]);                        break;
+        case 'archive':     await archive();                            break;
+
         case 'status':      await genStatus();                          break;
+        
+        case 'games':       await gameAmnt();                           break;
+        
         case 'discord':     await webhookLog(args[3], args[4]);         break;
         case 'discordE':    await webhookEmbed(args[3], args[4]);       break;
-        case 'games':       await gameAmnt();                           break;
-        case 'newAcc':      await newAcc();                             break;
-        case 'newPlr':      await newPlayer();                          break;
-        case 'newGuild':    await newGuild();                           break;
-        case 'archive':     await archive();                            break;
+       
+        case 'names':       await cli.checkNames();                     break;
+       
+        case 'newAcc':      await cli.newAcc();                         break;
+        case 'newPlr':      await cli.newPlayer();                      break;
+        case 'newGuild':    await cli.newGuild();                       break;
     }
 }
 
---
+++ src/cli.js
@@ -0,0 +1,94 @@
+const { getUUID } = require('./mojangRequest');
+const { getAccountWins, getGuildFromPlayer } = require('./hypixelApi');
+const { stringNormal, stringDaily } = require('./listUtils'); 
+const utils = require('./utils');
+const args = process.argv;
+const logger = utils.logger;
+
+async function newAcc() {
+    let name = args[3];
+    let category = args[4];
+    let uuid = await getUUID(name);
+    let wins = await getAccountWins(uuid);
+    let acclist = require('../acclist.json');
+    if (acclist[category].find(acc=>acc.uuid == uuid)) {
+        logger.err("Refusing to add duplicate!");
+    } else if (wins < 50 && category == 'gamers') {
+        logger.err("Refusing to add account with under 50 wins to gamers!");
+    }else {
+        acclist[category].push({ name : name, wins : wins, uuid: uuid });
+        await utils.writeJSON('./acclist.json',acclist);
+        logger.out(`${name} with ${wins} wins added.`);
+    }
+}
+
+async function newPlayer() {
+    let name = args[3];
+    let alts = args.slice(4);
+
+    // construct object
+    let playerObj = { name : name , accs : alts };
+
+    // add object to list
+    let plrlist = require('../playerlist.json');
+    plrlist.push(playerObj);
+
+    // write new list
+    await utils.writeJSON('./playerlist.json', plrlist);
+    logger.out(`Player "${name}" has been added with ${alts.length} alts.`)
+}
+
+async function newGuild() {
+    let playerUUID = args[3];
+
+    // get data from hypixel
+    let gldInfo = JSON.parse(await getGuildFromPlayer(playerUUID));
+    
+    // create the actual guild object
+    let id = gldInfo.guild._id;
+    let name = gldInfo.guild.name;
+    let gldObj = { id : id, name : name};
+    
+    // add object to list
+    let gldLst = require('../guildlist.json');
+    gldLst.push(gldObj);
+
+    // write new list
+    await utils.writeJSON('./guildlist.json', gldlLst);
+    logger.out(`Guild "${name} has been added successfully.`);
+}
+
+async function logNormal(name) {
+    logger.out(await stringNormal(name));
+}
+
+async function logDaily(name) {
+    logger.out(await stringDaily(name));
+}
+
+async function checkNames() {
+    let acclist = require('../acclist.json');
+    let realAccs = require('../accounts.json');
+
+    for(let list in acclist) {
+        for (let acc of acclist[list]) {
+            real = realAccs.find(a=>a.uuid==acc.uuid);
+            if (real != undefined && acc.name.toLowerCase() != real.name.toLowerCase()) {
+                logger.out(`${acc.name} -> ${real.name}`)
+                acc.name = real.name;
+            }
+        }
+    }
+
+    await utils.writeJSON('./acclist.json', acclist);
+    logger.out("Name check complete");
+}
+
+module.exports = {
+    newAcc : newAcc,
+    newGuild : newGuild,
+    newPlayer : newPlayer,
+    logNormal : logNormal,
+    logDaily : logDaily,
+    checkNames : checkNames
+}
