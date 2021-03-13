3ac4a7d0a25049e8fa0320eea561933bc9da47e8
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-12 16:27:33 -0800

    add to player page

--- html/player.js
+++ html/player.js
@@ -1,4 +1,5 @@
 var playername = undefined;
+let urlParams = undefined;
 
 function setName(name) {
     playername = name;
@@ -20,16 +21,20 @@ function displayData(data) {
     setHtmlByName("fh-wins", "Farm hunt wins: " + data.farmhuntWins);
     setHtmlByName(
         "hitw",
-        "HITW: " + data.hitwQual + "q " + data.hitwFinal + "f"
+        "HITW: " + data.hitwQual + "Q " + data.hitwFinal + "F"
     );
     setHtmlByName("xp", "XP: " + data.xp);
     setHtmlByName("karma", "Karma: " + data.karma);
     setHtmlByName("name", "Name: " + data.name);
     setHtmlByName("uuid", "UUID: " + data.uuid);
-    setHtmlByName(
-        "rank",
-        "Rank: " + data.rank.replace(/_/g, "").replace(/PLUS/g, "+")
-    );
+    if (data.rank) {
+        setHtmlByName(
+            "rank",
+            "Rank: " + data.rank.replace(/_/g, "").replace(/PLUS/g, "+")
+        );
+    } else {
+        setHtmlByName("rank", "Rank: Non");
+    }
     setHtmlByName("version", "Version: " + data.version);
     setHtmlByName("loggedIn", "Online: " + data.isLoggedIn);
     document
@@ -39,6 +44,18 @@ function displayData(data) {
             "https://crafatar.com/renders/body/" +
                 data.uuid +
                 "?size=512&default=MHF_Steve&scale=10&overlay"
+        );
+    document
+        .getElementById("lvlimg")
+        .setAttribute(
+            "src",
+            "https://gen.plancke.io/exp/" + data.uuid + ".png"
+        );
+    document
+        .getElementById("achivimg")
+        .setAttribute(
+            "src",
+            "https://gen.plancke.io/achievementPoints/" + data.uuid + ".png"
         );
 }
 
@@ -52,8 +69,26 @@ function loadData() {
                 );
                 if (playerdata != undefined) {
                     displayData(playerdata);
+                    if (
+                        urlParams.get("q").toLowerCase() !=
+                        playername.toLowerCase()
+                    ) {
+                        urlParams.set("q", playername);
+                        window.location.search = urlParams;
+                    }
                 }
             });
         });
     }
 }
+
+function loadPage() {
+    let queryString = window.location.search;
+    urlParams = new URLSearchParams(queryString);
+    playername = urlParams.get("q");
+
+    refresh();
+    document.getElementById("ign").value = urlParams.get("q");
+}
+
+window.addEventListener("load", loadPage);
