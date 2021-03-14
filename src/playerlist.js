6eeb6fbecb400bfbf6c9b598ceadbb336216cf61
eatmyvenom <eat.my.venomm@gmail.com>
2021-03-13 22:06:19 -0800

    add docs

--- src/playerlist.js
+++ src/playerlist.js
@@ -2,7 +2,7 @@
  * Wrapper around the list parser players function
  *
  * @param {*} accs
- * @return {Player[]} 
+ * @return {Player[]}
  */
 module.exports = function plrs(accs) {
     return require("./listParser").players(accs);
