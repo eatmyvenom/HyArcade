3ee2dad70340d937443e2cd22c86f1af0dda1fec
eatmyvenom <eat.my.venomm@gmail.com>
2021-04-10 02:36:01 -0700

    convert GetDataRaw to embed

--- src/discord/Commands/GetDataRaw.js
+++ src/discord/Commands/GetDataRaw.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const Command = require("../../classes/Command");
 const utils = require("../../utils");
 const BotUtils = require("../BotUtils");
@@ -6,6 +7,9 @@ module.exports = new Command("getDataRaw", ["*"], async (args, rawMsg) => {
     let plr = args[0];
     let acc = await BotUtils.resolveAccount(plr, rawMsg);
     let path = args[args.length - 1];
-    let res = acc.name + "." + path + " : " + acc[path];
-    return { res: res };
+    let embed = new MessageEmbed()
+        .setTitle(acc.name + "." + path)
+        .setDescription(acc[path])
+        .setColor(0x44a3e7)
+    return { res: "", embed :embed };
 });
