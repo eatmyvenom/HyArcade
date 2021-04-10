a87cf92367acbca5afba495e9683bdbeebfdbabd
eatmyvenom <eat.my.venomm@gmail.com>
2021-04-10 02:40:47 -0700

    link command use embeds

--- src/discord/Commands/Link.js
+++ src/discord/Commands/Link.js
@@ -1,13 +1,13 @@
+const { MessageEmbed } = require("discord.js");
 const Command = require("../../classes/Command");
-const mojangRequest = require("../../mojangRequest");
 const utils = require("../../utils");
 
 module.exports = new Command("link", utils.defaultAllowed, async (args) => {
     let player = args[0];
     let discord = args[1];
     let uuid;
-    let acclist = await utils.readJSON("./accounts.json");
     let acc;
+    let acclist = await utils.readJSON("./accounts.json");
     if (player.length < 17) {
         acc = acclist.find((a) => a.name.toLowerCase() == player.toLowerCase());
     } else {
@@ -15,19 +15,35 @@ module.exports = new Command("link", utils.defaultAllowed, async (args) => {
     }
 
     if (acc == undefined) {
-        return { res: "This player is not in the database!" };
+        let embed = new MessageEmbed()
+            .setTitle("ERROR")
+            .setDescription("This player is not in the database!")
+            .setColor(0xff0000)
+        return { res: "", embed : embed };
     }
 
     uuid = acc.uuid;
 
     let disclist = await utils.readJSON("./disclist.json");
     if (disclist[discord]) {
-        return { res: "This player has already been linked!" };
+        let embed = new MessageEmbed()
+            .setTitle("ERROR")
+            .setDescription("This player has already been linked!")
+            .setColor(0xff0000)
+        return { res: "", embed : embed };
     } else if (Object.values(disclist).find((u) => u == uuid) != undefined) {
-        return { res: "This user has already been linked!" };
+        let embed = new MessageEmbed()
+            .setTitle("ERROR")
+            .setDescription("This user has already been linked!")
+            .setColor(0xff0000)
+        return { res: "", embed : embed };
     }
 
     disclist[discord] = uuid;
     await utils.writeJSON("./disclist.json", disclist);
-    return { res: `${player} linked successfully!` };
+    let embed = new MessageEmbed()
+            .setTitle("Success")
+            .setDescription(`${player} linked successfully!`)
+            .setColor(0x00d492)
+    return { res: "", embed : embed };
 });
