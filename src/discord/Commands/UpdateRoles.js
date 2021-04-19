625cf81e8cee63fdd8f74035a23d39c9adcea003
eatmyvenom <eat.my.venomm@gmail.com>
2021-04-19 02:43:43 -0700

    ficks

--- src/discord/Commands/UpdateRoles.js
+++ src/discord/Commands/UpdateRoles.js
@@ -1,5 +1,6 @@
 const Command = require("../../classes/Command");
 const BotUtils = require("../BotUtils");
+const roleHandler = require("../roleHandler");
 
 module.exports = new Command('UpdateRoles', ['156952208045375488'], async (args)=> {
     await roleHandler(BotUtils.client);
