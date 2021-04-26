18d5461e0aa2457f128cf5a3cf9c9f181f0ac7d2
eatmyvenom <eat.my.venomm@gmail.com>
2021-04-26 16:04:09 -0700

    add more guilds to role update

--- src/discord/roleHandler.js
+++ src/discord/roleHandler.js
@@ -44,8 +44,33 @@ module.exports = async function roleHandler(client) {
         new Role(0, '808083629964001350'),
     ], "hypixelSaysWins");
 
+    let toGuild = await client.guilds.fetch('809959535174352946');
+    let TO = new RoleUpdater(toGuild, [
+        new Role(1000, '815629891034546216'),
+        new Role(500, '815629891395518485'),
+        new Role(300, '815629894453166100'),
+        new Role(100, '815629895303430184'),
+        new Role(50, '815629895303430184'),
+    ], "throwOutWins");
+
+    let simGuild = await client.guilds.fetch('826906189404176415');
+    let SIM = new RoleUpdater(simGuild, [
+        new Role(5000, '826929696443859014'),
+        new Role(2500, '826929634352168960'),
+        new Role(1000, '826929551308226580'),
+        new Role(500, '826929551308226580'),
+        new Role(250, '826929365648146483'),
+        new Role(100, '826929192062681099')
+   ], "simTotal")
+
+    logger.out('Updating roles for Party gamers \n')
     await PG.updateAll();
-    await HS.updateAll();
+    // logger.out('\nUpdating roles for Hypixel Says \n')
+    // await HS.updateAll();
+    logger.out('\nUpdating roles for Throw out\n')
+    await TO.updateAll();
+    logger.out('\nUpdating roles for sim games \n')
+    await SIM.updateAll();
     logger.out('Roles updated');
 
 }
