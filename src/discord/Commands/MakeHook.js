cf198b970e1f0970c97f075af86ed62a70e7fe8d
eatmyvenom <eat.my.venomm@gmail.com>
2021-05-06 00:31:14 -0700

    update bot avatar

--- src/discord/Commands/MakeHook.js
+++ src/discord/Commands/MakeHook.js
@@ -6,6 +6,6 @@ module.exports = new Command("mkhook", ["156952208045375488"], async (args) => {
     let channel = await BotUtils.client.channels.fetch(channelID);
     await channel.createWebhook("Arcade Bot Hook", {
         avatar:
-            "https://cdn.discordapp.com/avatars/818719828352696320/e3d2cac7292077850196fe232f1e7efe.webp",
+            "https://cdn.discordapp.com/avatars/818719828352696320/bb430aeea67244e5c2c8ab56dad79194.webp",
     });
 });
