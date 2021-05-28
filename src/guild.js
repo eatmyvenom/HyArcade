ff1f08fa311d36041266d3304ce3f92f3c0e1c8f
eatmyvenom <eat.my.venomm@gmail.com>
2021-05-28 07:44:30 -0700

    Add more stats to guilds

--- src/guild.js
+++ src/guild.js
@@ -1,6 +1,10 @@
 const hypixelApi = require("./hypixelApi");
 let accounts = [];
 
+function numberify(str) {
+    return Number(("" + str).replace(/undefined/g, 0).replace(/null/g, 0))
+}
+
 module.exports = function Gld(acclist) {
     accounts = acclist;
     return Guild;
@@ -66,12 +70,43 @@ class Guild {
         this.arcadeCoins = 0;
         this.combinedAP = 0;
         this.arcadeWins = 0;
+        this.karma = 0;
+	this.ranksGifted = 0;
+	this.hitwWins = 0;
+	this.farmhuntWins = 0;
+	this.miniWallsWins = 0;
+	this.footballWins - 0;
+	this.enderSpleefWins = 0;
+	this.throwOutWins = 0;
+	this.galaxyWarsWins = 0;
+	this.dragonWarsWins = 0;
+	this.bountyHuntersWins = 0;
+	this.blockingDeadWins = 0;
+	this.hideAndSeekWins = 0;
+	this.zombiesWins = 0;
+	this.pixelPaintersWins = 0;
+	this.simWins = 0;
         for (let i = 0; i < this.members.length; i++) {
             let member = this.members[i];
             this.wins += member.wins;
             this.arcadeCoins += member.arcadeCoins;
             this.combinedAP += member.achievementPoints;
             this.arcadeWins += member.combinedArcadeWins;
+            this.karma += numberify(member.karma);
+            this.ranksGifted += numberify(member.ranksGifted);
+            this.hitwWins += numberify(member.hitwWins);
+            this.farmhuntWins += numberify(member.farmhuntWins);
+            this.miniWallsWins += numberify(member.miniWallsWins);
+            this.footballWins += numberify(member.footballWins);
+            this.enderSpleefWins += numberify(member.enderSpleefWins);
+            this.throwOutWins += numberify(member.throwOutWins);
+            this.galaxyWarsWins += numberify(member.galaxyWarsWins);
+            this.dragonWarsWins += numberify(member.dragonWarsWins);
+            this.blockingDeadWins += numberify(member.blockingDeadWins);
+            this.hideAndSeekWins += numberify(member.hideAndSeekWins);
+            this.zombiesWins += numberify(member.zombiesWins);
+            this.pixelPaintersWins += numberify(member.pixelPaintersWins);
+            this.simWins += numberify(member.simTotal);
         }
         this.members = undefined;
         return this.wins;
