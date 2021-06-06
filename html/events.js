e21a3dadd1a0427ebbcca807882404b6ae4800c0
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-05 21:59:51 -0700

    add events page

---
+++ html/events.js
@@ -0,0 +1,40 @@
+let maxLength = 25;
+let interval;
+
+async function load() {
+    await refresh();
+    clearInterval(interval);
+    interval = setInterval(refresh, 25000)
+}
+
+async function refresh() {
+    let time = document.querySelector("time");
+    let servertime = await fetch("https://hyarcade.xyz/resources/timeupdate", {cache : "no-store"});
+    servertime = await servertime.text();
+    let formatted = new Date(servertime);
+    time.innerHTML = "Last database update : " + formatted.toLocaleTimeString();
+    let events = await fetch("https://hyarcade.xyz/resources/accounts.json", { cache: "no-store" });
+    events = await events.text();
+    await formatPage(events);
+}
+
+async function formatPage(events) {
+    events = JSON.parse(events);
+    let content = document.getElementById("evtWrapper");
+    let newContent = "";
+    for(let evt of events) {
+        newContent += evt[1];
+    }
+    content.innerHTML = newContent;
+} 
+
+function formatNum(number) {
+    let str = new Number(number);
+    if (number == undefined) {
+        return new Number(0).toLocaleString();
+    } else {
+        return str.toLocaleString();
+    }
+}
+
+window.addEventListener("load", load);
