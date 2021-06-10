3d6da7b5a8b4f5d26ddd7f3267b7a1d3168ccda8
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-10 00:50:14 -0700

    gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg

--- src/discord/messageHandler.js
+++ src/discord/messageHandler.js
@@ -161,12 +161,12 @@ module.exports = async function messageHandler(msg) {
     if (msg.webhookID) return;
     if (msg.guild.id == '808077828842455090') return;
     if (BotUtils.botMode == "mw") {
-        if(msg.guild.id == '789718245015289886') {
+        // if(msg.guild.id == '789718245015289886') {
             await mwMode(msg);
+            // return;
+        // } else {
             return;
-        } else {
-            return;
-        }
+        // }
     }
     if(msg.channel.id == '791122377333407784') await miniWallsVerify(msg);
     if(msg.channel.id == '742761029586649148') await pgVerify(msg);
