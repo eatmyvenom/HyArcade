3be7290547afeb83673a1d1284942f8035fef9f1
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-15 15:07:42 -0700

    proper file cache

--- src/discord/Commands/EZ.js
+++ src/discord/Commands/EZ.js
@@ -2,7 +2,7 @@ const Command = require("../../classes/Command");
 const BotUtils = require("../BotUtils");
 
 module.exports = new Command("ez", ["*"], async (args) => {
-    let msgs = await BotUtils.client.fileCache.ezmsgs;
+    let msgs = await BotUtils.fileCache.ezmsgs;
     let msg = msgs[(Math.floor(Math.random() * msgs.length))];
     return { res : msg };
 });
