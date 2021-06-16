9917cae59e7c2a93ffbe5ed8c4eee3da1f36be2e
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-15 23:08:45 -0700

    change alot of help text

--- src/discord/HelpText.js
+++ src/discord/HelpText.js
@@ -1,23 +1,25 @@
-exports.help = `Get info on how to use commands and what they do. 
+exports.help = `\`/arcadehelp [topic]\`
+Get info on how to use commands and what they do. 
 Using this with another argument for a command will give more data on how to use the command`;
 
-exports.verify = `Link your discord to your account
+exports.verify = `\`/verify [player]\`
+Link your discord to your account
 
-To link your discord first has to be set in your hypixel social media.
-To set this do the following:
+To link, your discord first has to be set in your hypixel social media.
+
+To set this value do the following on hypixel:
 Go to lobby -> Click the player head -> Click "Social Media" -> Click discord -> put your discord tag in the chat.`;
 
-exports.link = `Link a minecraft account to a discord ID. This is only for trusted users as it means that any discord account can be linked to any minecraft account
+exports.link = `\`/link [player] [discord]\`
+Link a minecraft account to a discord ID. This is only for trusted users as it means that any discord account can be linked to any minecraft account
 
 The linking process takes some time to merge a players discord data with their hypixel data, please allow 5-10 minutes of time before complaining about it.`;
 
-exports.newacc = `Add an account by name or uuid to the database. This should not be used for just random people you find, if you do that and add people who will just end up slowing down the whole system you will be blacklisted from using the bot.`;
+exports.newacc = `\`/addaccount [accounts]\`
+Add account(s) by name or uuid to the database. This should not be used for just random people you find, if you do that and add people who will just end up slowing down the whole system you will be blacklisted from using the bot.`;
 
-exports.stats = `Give the general arcade stats of a player. If the arcade game argument is filled then it also adds stats from that game. If something looks wrong thats probably because it is, the hypixel api isn't exactly known for providing accurate data. I apologize if this is the case but there is not much at all that can be done about it.`;
-
-exports.unlinkedstats = `Give the general arcade stats of a player not in the database. Please don't use this over and over, it slows down the database from updating which is annoying to say the least.`;
-
-exports.status = `Get the current status of a player in the database.`;
+exports.stats = `\`/stats [player] [game]\`
+Give the general arcade stats of a player. If the arcade game argument is filled then it also adds stats from that game. If something looks wrong thats probably because it is, the hypixel api isn't exactly known for providing accurate data. I apologize if this is the case but there is not much at all that can be done about it.`;
 
 exports.lb = `Get the current leaderboard from most arcade games, type refers to a time type, this can be either "daily" "weekly" "monthly" or "lifetime" with shorter version available.
 
