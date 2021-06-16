7f9c4f21c57ca027fb6a9e2dbbeff05adbf5ede7
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-15 22:36:27 -0700

    [object Object]

--- src/dataGeneration.js
+++ src/dataGeneration.js
@@ -204,7 +204,7 @@ async function updateAllAccounts() {
             let acc = accounts[i];
             let newAcc = fullList.find(a=>a.uuid==acc.uuid);
             if(newAcc.xp > acc.xp) {
-                logger.out(`Setting ${newAcc}'s data from outside source!`);
+                logger.out(`Setting ${newAcc.name}'s data from outside source!`);
                 acc.setData(newAcc);
             }
         }
