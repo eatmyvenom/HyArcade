e1f0af8c14851f5d4c37d35b4bb6424f57890516
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-15 23:42:24 -0700

    use file cache for role updates

--- src/discord/RoleUpdater.js
+++ src/discord/RoleUpdater.js
@@ -23,8 +23,8 @@ module.exports = class RoleUpdater {
     }
 
     async updateAll() {
-        let disclist = await utils.readJSON("disclist.json");
-        let acclist = await utils.readJSON("accounts.json");
+        let disclist = await BotUtils.fileCache.disclist;
+        let acclist = await BotUtils.fileCache.acclist;
         let mbrList = await this.guild.members.fetch();
         for (let discid in disclist) {
             if (mbrList.has(discid)) {
