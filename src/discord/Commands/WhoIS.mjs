364509eb757af45eb356b76b12f026e94d2b16f2
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-17 21:13:14 -0700

    experiment with emca modules

--- src/discord/Commands/WhoIS.mjs
+++ src/discord/Commands/WhoIS.mjs
@@ -2,9 +2,11 @@ const { MessageEmbed } = require("discord.js");
 const Command = require("../../classes/Command");
 const BotUtils = require("../BotUtils");
 
-module.exports = new Command("whois", ["*"], async (args, rawMsg) => {
+let WhoIS = new Command("whois", ["*"], async (args, rawMsg) => {
     let plr = args[0];
     let acc = await BotUtils.resolveAccount(plr, rawMsg, args.length != 2);
     let embed = new MessageEmbed().setTitle(`${acc.name} discord`).setDescription(`Discord ID: ${acc.discord}\n<@${acc.discord}>`).setColor(0x44a3e7);
     return { res: "", embed: embed };
 });
+
+export { WhoIS };
