17cad6f267847def0cc08c6dad746c68db00a5c7
eatmyvenom <eat.my.venomm@gmail.com>
2021-06-19 00:31:00 -0700

    ugly ass div

--- html/status.js
+++ html/status.js
@@ -1,19 +1,19 @@
 async function load() {
     let status = await fetch("https://hyarcade.xyz/resources/serverStatus.json", { cache: "no-store" });
     status = await status.json();
-    let div = document.querySelector("div");
+    let main = document.querySelector("main");
     let mw = (status.mw) ? "Online" : "Inactive";
     let arc = (status.arc) ? "Online" : "Inactive";
     let interact = (status.slash) ? "Online" : "Inactive";
     let db = (status.database) ? "Operational" : "Corrupted";
-    div.innerHTML = `Hypixel status : ${status.Hypixel}\n`;
-    div.innerHTML += `Mojang session servers : ${status.MSession}\n`;
-    div.innerHTML += `Mojang account servers : ${status.MAcc}\n`;
-    div.innerHTML += `Mojang auth servers: ${status.MAuth}\n`;
-    div.innerHTML += `Mini walls bot : ${mw}\n`;
-    div.innerHTML += `Arcade bot : ${arc}\n`;
-    div.innerHTML += `Interactions : ${interact}\n`;
-    div.innerHTML += `Database : ${db}`;
+    main.innerHTML = `Hypixel status : ${status.Hypixel}\n`;
+    main.innerHTML += `Mojang session servers : ${status.MSession}\n`;
+    main.innerHTML += `Mojang account servers : ${status.MAcc}\n`;
+    main.innerHTML += `Mojang auth servers: ${status.MAuth}\n`;
+    main.innerHTML += `Mini walls bot : ${mw}\n`;
+    main.innerHTML += `Arcade bot : ${arc}\n`;
+    main.innerHTML += `Interactions : ${interact}\n`;
+    main.innerHTML += `Database : ${db}`;
 }
 
 window.addEventListener("load", load);
