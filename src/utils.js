6b9f4938c359cc2b36493e8834a2021e8cfea725
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-07 00:37:05 -0700

    g

--- src/utils.js
+++ src/utils.js
@@ -67,7 +67,7 @@ function day() {
  * @param {Object} json the json data
  */
 async function writeJSON(path, json) {
-    await BSONwriter(path, json);
+    // await BSONwriter(path, json);
     await fs.writeFile("data/" + path, JSON.stringify(json, null, 4));
     try {
         await readJSON(path);
