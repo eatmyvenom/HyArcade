8025267d7fa9ce3d33d50abbe266a8905632cb9b
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-06 22:22:59 -0700

    just .bson extention not .json

--- src/utils/files/BSONreader.js
+++ src/utils/files/BSONreader.js
@@ -2,6 +2,6 @@ const BSON = require("bson");
 const fs = require("fs-extra");
 
 module.exports = async function BSONreader(path) {
-    let buffer = await fs.readFile(path);
+    let buffer = await fs.readFile(("" + path).replace(/json/g, "bson"));
     return BSON.deserialize(buffer);
 }
--- src/utils/files/BSONwriter.js
+++ src/utils/files/BSONwriter.js
@@ -3,5 +3,5 @@ const fs = require("fs-extra");
 
 module.exports = async function BSONwriter(path, object) {
     let buffer = BSON.serialize(object);
-    await fs.writeFile(path, buffer);
+    await fs.writeFile(("" + path).replace(/json/g, "bson"), buffer);
 }
