43dbc8bdc5087b7459bf3bad8b0ad714dbbd3351
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-08 10:43:56 -0700

    log bots dying

--- spawn-bots.js
+++ spawn-bots.js
@@ -1,5 +1,6 @@
 #!/bin/env node
 const child_process = require("child_process");
+const Logger = require("./src/utils/Logger");
 
 /**
  * @type {child_process.ChildProcess}
@@ -43,21 +44,25 @@ async function main() {
 }
 
 function restartMW() {
+    Logger.error("Mini walls bot crashed!");
     mw = child_process.fork("index.js", ["bot", "mini"], { silent : false});
     mw.on("exit", restartMW);
 }
 
 function restartMini() {
+    Logger.error("Mini bot crashed!");
     mini = child_process.fork("index.js", ["bot", "mini"], { silent : false});
     mini.on("exit", restartMini);
 }
 
 function restartArcade() {
+    Logger.error("Arcade bot crashed!");
     arcade = child_process.fork("index.js", ["bot"], { silent : false});
     arcade.on("exit", restartArcade);
 }
 
 function restartInteraction() {
+    Logger.error("Interactions has crashed!");
     interactions = child_process.fork("index.js", ["bot", "slash"], { silent : false});
     interactions.on("exit", restartInteraction);
 }
