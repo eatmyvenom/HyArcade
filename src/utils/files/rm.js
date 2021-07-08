b281a45b132ea46c3292d9bce85ef673b23e88ed
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-08 01:08:12 -0700

    add rm with support for data and bson

---
+++ src/utils/files/rm.js
@@ -0,0 +1,17 @@
+const fs = require("fs-extra");
+
+module.exports = async function rm(path) {
+    if(fs.existsSync(path)) {
+        await fs.rm(path);
+        return true;
+    } else if (fs.existsSync("data/" + path)) {
+        await fs.rm("data/" + path);
+        return true;
+    } else if (fs.existsSync("data/" + path + ".1")) {
+        await fs.rm("data/" + path + ".1");
+        await fs.rm("data/" + path + ".2");
+        return true;
+    } else {
+        return false;
+    }
+}
