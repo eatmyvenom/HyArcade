5ec1da45004711f2edfdcb94c0fc9bc5f1f48a63
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-15 03:48:12 -0700

    dont double defer

--- src/discord/interactions/Utils/AccountResolver.js
+++ src/discord/interactions/Utils/AccountResolver.js
@@ -7,7 +7,6 @@ const BotUtils = require("../../BotUtils");
 const cfg = require("../../../Config").fromJSON();
 
 async function getFromHypixel(string, interaction) {
-    await interaction.defer();
     logger.info("Unable to resolve, getting by ign from hypixel.");
 
     let plr = string;
