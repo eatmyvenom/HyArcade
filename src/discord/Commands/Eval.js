8e428df4385632dd563e220dfdab37172502efbb
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-16 17:20:00 -0700

    improve eval command

--- src/discord/Commands/Eval.js
+++ src/discord/Commands/Eval.js
@@ -1,12 +1,13 @@
 const Command = require("../../classes/Command");
+const { getFromDB } = require("../BotUtils");
 const BotUtils = require("../BotUtils");
 
 function safeEval(str) {
-    return Function('"use strict";return (' + str + ')');
+    return Function("c", "r", "bu", "accs", "m", '"use strict";return (' + str + ')');
 }
 
-module.exports = new Command("eval", ["156952208045375488"], async (args) => {
+module.exports = new Command("eval", ["156952208045375488"], async (args, rawMsg) => {
     let c = BotUtils.client;
     let f = safeEval(args.join(" "));
-    return { res : f()};
+    return { res : "```\nResponse:\n" + f(c, require, BotUtils, await getFromDB("accounts"), rawMsg) + "\n```"};
 });
