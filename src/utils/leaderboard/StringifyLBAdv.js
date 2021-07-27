b1c37d428b9084063465ce3fc3c6b75d02e21e95
eatmyvenom <eat.my.venomm@gmail.com>
2021-07-27 00:38:55 -0700

    add a bunch of leaderboards. fixes #54

--- src/utils/leaderboard/StringifyLBAdv.js
+++ src/utils/leaderboard/StringifyLBAdv.js
@@ -4,13 +4,13 @@ function formatNum(number) {
     return Intl.NumberFormat("en").format(number);
 }
 
-module.exports = async function stringLBAdv(comparitor, parser, maxamnt, listTransformer) {
+module.exports = async function stringLBAdv(comparitor, parser, maxamnt, listTransformer, startingIndex = 0) {
     let list = await getList();
     list = await listTransformer(list);
     list = list.sort(comparitor);
 
     let str = "";
-    list = list.slice(0, maxamnt);
+    list = list.slice(startingIndex, maxamnt);
     for (let i = 0; i < list.length; i++) {
         let propVal = parser(list[i]);
 
