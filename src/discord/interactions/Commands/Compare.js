2d591504ad4ed5fd0505e301c4813331762fd265
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-03 16:33:53 -0700

    add `/compare` choices

--- src/discord/interactions/Commands/Compare.js
+++ src/discord/interactions/Commands/Compare.js
@@ -18,6 +18,79 @@ module.exports = {
         type: "STRING",
         description: "The game you would like to compare the players in",
         required: true,
+        choices: [{
+            name: "Overall",
+            value: "arc",
+        },
+        {
+            name: "Party games",
+            value: "pg",
+        },
+        {
+            name: "Farm hunt",
+            value: "fh",
+        },
+        {
+            name: "Hole in the wall",
+            value: "hitw",
+        },
+        {
+            name: "Hypixel says",
+            value: "hs",
+        },
+        {
+            name: "Blocking dead",
+            value: "bd",
+        },
+        {
+            name: "Mini walls",
+            value: "mw",
+        },
+        {
+            name: "Football",
+            value: "fb",
+        },
+        {
+            name: "Ender spleef",
+            value: "es",
+        },
+        {
+            name: "Throw out",
+            value: "to",
+        },
+        {
+            name: "Galaxy wars",
+            value: "gw",
+        },
+        {
+            name: "Dragon wars",
+            value: "dw",
+        },
+        {
+            name: "Bounty hunters",
+            value: "bh",
+        },
+        {
+            name: "Hide and seek",
+            value: "hns",
+        },
+        {
+            name: "Zombies",
+            value: "z",
+        },
+        {
+            name: "Pixel painters",
+            value: "pp",
+        },
+        {
+            name: "Capture the wool",
+            value: "ctw",
+        },
+        {
+            name: "Seasonal games",
+            value: "sim",
+        },
+        ]
     },
     ],
 };
