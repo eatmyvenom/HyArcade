1e1b372daa0ce7de2fa00e1744903b2ba3bbe3fe
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-09 19:26:40 -0700

    diable eslint in tests

--- tests/AccountBreakage.js
+++ tests/AccountBreakage.js
@@ -1,12 +1,13 @@
+/* eslint-disable */
+
 const Account = require("../src/request/types/Account");
 
-/**
- * Test if "bloozing" works correctly
- */
 async function main () {
     let a = new Account("bigc1109", 0, "f0133668f8da4ab7ad3e62ca1b5574da");
     await a.updateData();
-    console.log(a.arcadeAchievments.miniWalls);
+    console.log(a.arcadeAchievments.captureTheWool);
 }
 
-main();
+main()
+    .then(console.log)
+    .catch(console.error);
