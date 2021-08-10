7123823fba720efeef7f48e9e2278324cf6b123a
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-09 20:35:29 -0700

    fix indentation

--- src/gamesPlayed.js
+++ src/gamesPlayed.js
@@ -6,42 +6,42 @@ const {
 } = require("./utils");
 
 class gamesPlayed {
-    uuid = "";
-    counts = {};
-    newestTime = 0;
+  uuid = "";
+  counts = {};
+  newestTime = 0;
 
-    constructor (uuid, oldCounts, newestTime) {
-      this.uuid = uuid;
-      this.counts = oldCounts;
-      this.newestTime = newestTime;
-    }
+  constructor (uuid, oldCounts, newestTime) {
+    this.uuid = uuid;
+    this.counts = oldCounts;
+    this.newestTime = newestTime;
+  }
 
-    async updateData () {
-      const json = JSON.parse(await hypixelApi.getGamesPlayedRAW(this.uuid));
-      const newGames = json.games.reverse();
+  async updateData () {
+    const json = JSON.parse(await hypixelApi.getGamesPlayedRAW(this.uuid));
+    const newGames = json.games.reverse();
 
-      for(const g of newGames) {
-        const game = new Game(g);
-        if(game.start > this.newestTime) {
-          this.addCount(game.type);
-          this.newestTime = game.start;
-        }
+    for(const g of newGames) {
+      const game = new Game(g);
+      if(game.start > this.newestTime) {
+        this.addCount(game.type);
+        this.newestTime = game.start;
       }
     }
+  }
 
-    addCount (type) {
-      if(this.counts[type] != undefined) {
-        this.counts[type] += 1;
-      } else {
-        this.counts[type] = 1;
-      }
+  addCount (type) {
+    if(this.counts[type] != undefined) {
+      this.counts[type] += 1;
+    } else {
+      this.counts[type] = 1;
     }
+  }
 
-    async writeToFile () {
-      const full = await readJSON("./gamesPlayed.json");
-      full[this.uuid] = this;
-      await writeJSON("./gamesPlayed.json", full);
-    }
+  async writeToFile () {
+    const full = await readJSON("./gamesPlayed.json");
+    full[this.uuid] = this;
+    await writeJSON("./gamesPlayed.json", full);
+  }
 }
 
 module.exports = gamesPlayed;
