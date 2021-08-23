074697324d018b018e66dae6a9ad01ad58e292f6
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-22 21:14:47 -0700

    lint whois

--- src/discord/Commands/WhoIS.mjs
+++ src/discord/Commands/WhoIS.mjs
@@ -5,17 +5,17 @@ import CommandResponse from "../Utils/CommandResponse.js";
 import { INFO_WHOIS } from "../Utils/Embeds/DynamicEmbeds.js";
 import { ERROR_UNLINKED } from "../Utils/Embeds/StaticEmbeds.js";
 
-export let WhoIS = new Command("whois", ["*"], async (args, rawMsg, interaction) => {
-    let plr = args[0];
-    let acc;
-    if (interaction == undefined) {
-        acc = await BotUtils.resolveAccount(plr, rawMsg, args.length != 2);
-    } else {
-        acc = await InteractionUtils.resolveAccount(interaction);
-        if(acc == undefined) {
-            return new CommandResponse("", ERROR_UNLINKED);
-        }
+export const WhoIS = new Command("whois", ["*"], async (args, rawMsg, interaction) => {
+  const plr = args[0];
+  let acc;
+  if (interaction == undefined) {
+    acc = await BotUtils.resolveAccount(plr, rawMsg, args.length != 2);
+  } else {
+    acc = await InteractionUtils.resolveAccount(interaction);
+    if(acc == undefined) {
+      return new CommandResponse("", ERROR_UNLINKED);
     }
-    let embed = INFO_WHOIS(acc);
-    return { res: "", embed: embed };
+  }
+  const embed = INFO_WHOIS(acc);
+  return { res: "", embed };
 });
