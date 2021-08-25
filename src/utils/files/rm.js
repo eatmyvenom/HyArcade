437618b1d1ccc8867b33f30d5782fe2d85881d8b
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-25 12:32:55 -0700

    improve rm tool

--- src/utils/files/rm.js
+++ src/utils/files/rm.js
@@ -11,6 +11,14 @@ module.exports = async function rm (path) {
     await fs.rm(`data/${path}.1`);
     await fs.rm(`data/${path}.2`);
     return true;
+  } else if(fs.existsSync(`data/${path}.bson.1`)) {
+    await fs.rm(`data/${path}.1`);
+    await fs.rm(`data/${path}.2`);
+    return true;
+  } else if(fs.existsSync(`data/${path}.json.1`)) {
+    await fs.rm(`data/${path}.1`);
+    await fs.rm(`data/${path}.2`);
+    return true;
   }
   return false;
 
