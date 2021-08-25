a5dd605b9c8e71f3977a6f44aa08572dcc4d12e2
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-25 16:59:05 -0700

    lint verify

--- src/discord/Commands/LinkMe.mjs
+++ src/discord/Commands/LinkMe.mjs
@@ -5,66 +5,66 @@ import { addAccounts } from "../../listUtils.js";
 import BotUtils from "../BotUtils.js";
 import { ERROR_ACCOUNT_PREVIOUSLY_LINKED, ERROR_IGN_UNDEFINED, ERROR_INPUT_IGN, ERROR_LINK_HYPIXEL_MISMATCH, ERROR_PLAYER_PREVIOUSLY_LINKED, INFO_LINK_SUCCESS } from "../Utils/Embeds/StaticEmbeds.js";
 
-export let Verify = new Command("linkme", ["*"], async (args, rawMsg, interaction) => {
-    let player = args[0];
-    if (player == undefined) {
-        let embed = ERROR_INPUT_IGN;
-        return { res: "", embed: embed };
-    }
-    await interaction.defer();
-    let acclist = await BotUtils.getFromDB("accounts");
-    let acc = acclist.find(
-        (a) =>
-            ("" + a.uuid).toLowerCase() == player.toLowerCase() || ("" + a.name).toLowerCase() == player.toLowerCase()
-    );
-    if (acc == undefined) {
-        let uuid = player.length == 32 ? player : await mojangRequest.getUUID(player);
-        if (("" + uuid).length != 32) {
-            let noexistEmbed = ERROR_IGN_UNDEFINED;
+export const Verify = new Command("linkme", ["*"], async (args, rawMsg, interaction) => {
+  const player = args[0];
+  if (player == undefined) {
+    const embed = ERROR_INPUT_IGN;
+    return { res: "", embed };
+  }
+  await interaction.defer();
+  const acclist = await BotUtils.getFromDB("accounts");
+  let acc = acclist.find(
+    (a) =>
+      (`${a.uuid}`).toLowerCase() == player.toLowerCase() || (`${a.name}`).toLowerCase() == player.toLowerCase()
+  );
+  if (acc == undefined) {
+    const uuid = player.length == 32 ? player : await mojangRequest.getUUID(player);
+    if ((`${uuid}`).length != 32) {
+      const noexistEmbed = ERROR_IGN_UNDEFINED;
 
-            return { res: "", embed: noexistEmbed };
-        }
-        acc = new Account(player, 0, uuid);
-        await addAccounts("others", [uuid]);
-        await acc.updateHypixel();
+      return { res: "", embed: noexistEmbed };
     }
+    acc = new Account(player, 0, uuid);
+    await addAccounts("others", [uuid]);
+    await acc.updateHypixel();
+  }
 
-    let tag;
+  let tag;
+  if (interaction == undefined) {
+    tag = rawMsg.author.tag.toLowerCase();
+  } else {
+    tag = interaction.member.user.tag.toLowerCase();
+  }
+
+  if ((`${acc.hypixelDiscord}`).toLowerCase() == tag) {
+    let uuid = player;
+    // if its not a uuid then convert to uuid
+    if (player.length < 17) {
+      uuid = acc.uuid;
+    }
+    let discord;
     if (interaction == undefined) {
-        tag = rawMsg.author.tag.toLowerCase();
+      discord = rawMsg.author.id;
     } else {
-        tag = interaction.member.user.tag.toLowerCase();
+      discord = interaction.member.id;
+    }
+    const disclist = await BotUtils.getFromDB("disclist");
+    // make sure player isnt linked
+    if (disclist[discord]) {
+      const embed = ERROR_PLAYER_PREVIOUSLY_LINKED;
+      return { res: "", embed };
+      // make sure user isnt linked
+    } else if (Object.values(disclist).find((u) => u == uuid) != undefined) {
+      const embed = ERROR_ACCOUNT_PREVIOUSLY_LINKED;
+      return { res: "", embed };
     }
 
-    if (("" + acc.hypixelDiscord).toLowerCase() == tag) {
-        let uuid = player;
-        // if its not a uuid then convert to uuid
-        if (player.length < 17) {
-            uuid = acc.uuid;
-        }
-        let discord;
-        if (interaction == undefined) {
-            discord = rawMsg.author.id;
-        } else {
-            discord = interaction.member.id;
-        }
-        let disclist = await BotUtils.getFromDB("disclist");
-        // make sure player isnt linked
-        if (disclist[discord]) {
-            let embed = ERROR_PLAYER_PREVIOUSLY_LINKED;
-            return { res: "", embed: embed };
-            // make sure user isnt linked
-        } else if (Object.values(disclist).find((u) => u == uuid) != undefined) {
-            let embed = ERROR_ACCOUNT_PREVIOUSLY_LINKED;
-            return { res: "", embed: embed };
-        }
-
-        disclist[discord] = uuid;
-        await BotUtils.writeToDB("disclist", disclist);
-        let embed = INFO_LINK_SUCCESS;
-        return { res: "", embed: embed };
-    } else {
-        let embed = ERROR_LINK_HYPIXEL_MISMATCH;
-        return { res: "", embed: embed };
-    }
+    disclist[discord] = uuid;
+    await BotUtils.writeToDB("disclist", disclist);
+    const embed = INFO_LINK_SUCCESS;
+    return { res: "", embed };
+  } 
+  const embed = ERROR_LINK_HYPIXEL_MISMATCH;
+  return { res: "", embed };
+    
 });
