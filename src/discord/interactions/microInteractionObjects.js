7a1ffeddb5a3054daf2427f0a033c6a17f4d72e5
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-25 21:17:27 -0700

    rework micro bot command obj

--- src/discord/interactions/microInteractionObjects.js
+++ src/discord/interactions/microInteractionObjects.js
@@ -1,32 +1,13 @@
-const statsCommand = require("./Commands/Stats");
-statsCommand.options[0].required = true;
-exports.statsCommand = statsCommand;
-
+exports.statsCommand = require("./Commands/Stats");
 exports.compare = require("./Commands/Compare");
 exports.info = require("./Commands/Info");
 exports.gamecounts = require("./Commands/GameCounts");
-
-const namehistory = require("./Commands/NameHistory");
-namehistory.options[0].required = true;
-
-const profile = require("./Commands/Profile");
-profile.options[0].required = true;
-exports.profile = profile;
-
-const topgames = require("./Commands/TopGames");
-topgames.options = [topgames.options[0]];
-topgames.options[0].required = true;
-exports.topgames = topgames;
-
-const arcade = require("./Commands/Arcade");
-arcade.options = [arcade.options[0], arcade.options[1], arcade.options[2]];
-exports.arcade = arcade;
-
+exports.whois = require("./Commands/Whois");
+exports.profile = require("./Commands/Profile");
+exports.topgames = require("./Commands/TopGames");
+exports.arcade = require("./Commands/Arcade");
 exports.quake = require("./Commands/Quake");
 exports.arena = require("./Commands/Arena");
 exports.pball = require("./Commands/PBall");
 exports.walls = require("./Commands/Walls");
-
-const zombies = require("./Commands/Zombies");
-zombies.options[0].required = true;
-exports.zombies = zombies;
+exports.zombies = require("./Commands/TopGames");
