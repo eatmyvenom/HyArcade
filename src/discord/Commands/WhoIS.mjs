eb4f010e5f6cb2a40c064ac39978b0cd0cd8db72
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-29 14:35:57 -0700

    improve whois

--- src/discord/Commands/WhoIS.mjs
+++ src/discord/Commands/WhoIS.mjs
@@ -9,7 +9,19 @@ import CommandResponse from "../Utils/CommandResponse.js";
 import { COLOR_PURPLE } from "../Utils/Embeds/Colors.js";
 import { ERROR_UNLINKED } from "../Utils/Embeds/StaticEmbeds.js";
 
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Util } = require("discord.js");
+const { escapeUnderline } = Util;
+
+/**
+ * 
+ * @param {number} n 
+ * @returns {string}
+ */
+function numberify (n) {
+  let r = Intl.NumberFormat("en").format(Number((`${n}`).replace(/undefined/g, 0).replace(/null/g, 0)));
+  r = isNaN(n) ? (r = "N/A") : r;
+  return r; 
+}
 
 /**
  * @param {Account} acc
@@ -17,22 +29,22 @@ const { MessageEmbed } = require("discord.js");
  */
 function getMain (acc) {
   const games = {
-    Party_games: acc.partyGames.wins,
-    HITW: acc.holeInTheWall.wins,
-    Farm_hunt: acc.farmhunt.wins,
-    Hypixel_says: acc.hypixelSays.wins,
-    Mini_walls: acc.miniWalls.wins,
-    Football: acc.football.wins,
-    Ender_spleef: acc.enderSpleef.wins,
-    Dragon_wars: acc.dragonWars.wins,
-    Galaxy_wars: acc.galaxyWars.wins,
-    Bounty_hunters: acc.bountyHunters.wins,
-    Blocking_dead: acc.blockingDead.wins,
-    Throw_out: acc.throwOut.wins,
-    Hide_and_seek: acc.hideAndSeek.wins,
-    Zombies: acc.zombies.wins_zombies,
-    Pixel_painters: acc.pixelPainters.wins,
-    Seasonal: acc.seasonalWins.total,
+    Party_games: acc?.partyGames?.wins ?? 0,
+    HITW: acc?.holeInTheWall?.wins ?? 0,
+    Farm_hunt: acc?.farmhunt?.wins ?? 0,
+    Hypixel_says: acc?.hypixelSays?.wins ?? 0,
+    Mini_walls: acc?.miniWalls?.wins ?? 0,
+    Football: acc?.football?.wins ?? 0,
+    Ender_spleef: acc?.enderSpleef?.wins ?? 0,
+    Dragon_wars: acc?.dragonWars?.wins ?? 0,
+    Galaxy_wars: acc?.galaxyWars?.wins ?? 0,
+    Bounty_hunters: acc?.bountyHunters?.wins ?? 0,
+    Blocking_dead: acc?.blockingDead?.wins ?? 0,
+    Throw_out: acc?.throwOut?.wins ?? 0,
+    Hide_and_seek: acc?.hideAndSeek?.wins ?? 0,
+    Zombies: acc?.zombies?.wins_zombies ?? 0,
+    Pixel_painters: acc?.pixelPainters?.wins ?? 0,
+    Seasonal: acc?.seasonalWins?.total ?? 0,
   };
 
   let max = 0;
@@ -44,7 +56,7 @@ function getMain (acc) {
     }
   }
 
-  return `${game.replace(/_/g, " ")}`;
+  return `${game.replace(/_/g, " ")}: **${numberify(max)} wins**`;
 }
 
 /**
@@ -57,19 +69,20 @@ function generateWhoisEmbed (acc) {
 
   const embed = new MessageEmbed()
     .setTitle(`Who is ${rank}${acc.name}`)
+    .setThumbnail(`https://crafatar.com/renders/body/${acc.uuid}?overlay&time=${Date.now()}`)
     .setColor(COLOR_PURPLE);
 
   let discord = "";
 
   if(acc.discord != undefined) {
-    discord = `<@${acc.discord}>\n**ID** - ${acc.discord}`;
+    discord = `**Ping** - <@${acc.discord}>\n**ID** - ${acc.discord}\n**Hypixel tag** - ${acc.hypixelDiscord}`;
   } else {
     discord = "Unknown!";
   }
 
-  embed.addField("--- Discord ---", discord, true);
-  embed.addField("---- Names ----", acc.nameHist.map((n, i) => `${i + 1} - **${n}**`).join("\n"), true);
-  embed.addField("---- Info -----", `**Guild** - ${acc.guild ?? "Unknown"}\n**Main** - ${getMain(acc)}\n**`, true);
+  embed.addField("--------- Names ---------", acc.nameHist.map((n, i) => `${i + 1} - **${escapeUnderline(n)}**`).join("\n"), true);
+  embed.addField("-------- Discord --------", discord, true);
+  embed.addField("--------- Info ----------", `**Guild** - ${acc.guild ?? "Unknown"}\n**Main** - ${getMain(acc)}`, false);
 
   return embed;
 }
