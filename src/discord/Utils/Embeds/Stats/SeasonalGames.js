2d8e039eeefc0086b7e1ab3cadf344d303a62b7f
eatmyvenom <eat.my.venomm@gmail.com>
2021-10-11 10:52:55 -0700

    update seasonal stats

--- src/discord/Utils/Embeds/Stats/SeasonalGames.js
+++ src/discord/Utils/Embeds/Stats/SeasonalGames.js
@@ -17,6 +17,7 @@ function f (number) {
  */
 module.exports = function SeasonalGames (acc, embed) {
   embed.addField("Stats", `**- Total Wins** (\`${f(acc?.seasonalWins?.total ?? 0)}\`)\n**- Easter Wins** (\`${f(acc.seasonalWins.easter)}\`)\n**- Grinch Wins** (\`${f(acc.seasonalWins.grinch)}\`)\n**- Halloween Wins** (\`${f(acc.seasonalWins.halloween)}\`)\n**- Scuba Wins** (\`${f(acc.seasonalWins.scuba)}\`)`);
+  embed.addField("Info", `**- Easter Found** (\`${f(acc.seasonalWins.foundEaster)}\`)\n**- Halloween Found** (\`${f(acc.seasonalWins.foundHalloween)}\`)\n**- Scuba Points** (\`${f(acc.seasonalWins.pointsScuba)}\`)`);
 
   return embed;
 };
