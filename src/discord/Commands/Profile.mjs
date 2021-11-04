9eba08827754ecb8a2f75efedcbcc76422435e77
eatmyvenom <eat.my.venomm@gmail.com>
2021-11-04 16:35:47 -0700

    rework /profile visuals

--- src/discord/Commands/Profile.mjs
+++ src/discord/Commands/Profile.mjs
@@ -49,7 +49,7 @@ function getMain (acc) {
     }
   }
 
-  return `${game.replace(/_/g, " ")} - ${numberify(max)}`;
+  return `&l${game.replace(/_/g, " ")}&r &7-&r ${numberify(max)}`;
 }
 
 export default new Command(["profile", "p", "arcprofile"], ["*"], async (args, rawMsg, interaction) => {
@@ -69,23 +69,23 @@ export default new Command(["profile", "p", "arcprofile"], ["*"], async (args, r
   const img = new ImageGenerator(1280, 800, "'myFont'", true);
   await img.addBackground("resources/arcblur.png", 0, 0, 1280, 800, "#0000005F");
 
-  img.writeText("Arcade Games Stats", 640, 40, "center", "#FFFFFF", "48px");
-  img.writeAcc(acc, undefined, 100, "48px");
+  img.writeText("Arcade Games Stats", 640, 40, "center", "#FFFFFF", "56px", 0, "'boldmc'");
+  img.writeAcc(acc, undefined, 110, "48px");
 
   const wins = `${numberify(Math.max(acc.arcadeWins, acc.combinedArcadeWins))}`;
   const ap = `${numberify(acc.arcadeAchievments.totalEarned)} / ${numberify(acc.arcadeAchievments.totalAvailiable)}`;
   const quests = `${Object.values(acc.quests).reduce((p, c) => p + c, 0)}`;
   const challenges = `${numberify(Object.values(acc.arcadeChallenges).reduce((p, c) => p + c, 0))}`;
 
-  img.writeText(`Total Wins - ${wins}`, 640, 300, "center", "#FFFF55", "40px");
+  img.writeText(`&lTotal Wins - ${wins}`, 640, 300, "center", "#FFFF55", "42px");
 
-  img.writeText(`AP - ${ap}`, 320, 400, "center", "#55FFFF", "40px");
-  img.writeText(`Quests - ${numberify(quests)}`, 320, 500, "center", "#FFAA00", "40px");
-  img.writeText(`Total Coins - ${numberify(acc.arcadeCoins)}`, 320, 600, "center", "#55FF55", "40px");
+  img.writeText(`&lAP&r &7-&r ${ap}`, 20, 400, "left", "#55FFFF", "42px");
+  img.writeText(`&lQuests&r &7-&r ${numberify(quests)}`, 20, 500, "left", "#FFAA00", "42px");
+  img.writeText(`&lTotal Coins&r &7-&r ${numberify(acc.arcadeCoins)}`, 20, 600, "left", "#55FF55", "42px");
 
-  img.writeText(`${getMain(acc)}`, 960, 400, "center", "#55FFFF", "40px");
-  img.writeText(`Challenges - ${challenges}`, 960, 500, "center", "#FFAA00", "40px");
-  img.writeText(`Coins Earned - ${numberify(acc.coinsEarned)}`, 960, 600, "center", "#55FF55", "40px");
+  img.writeText(`${getMain(acc)}`, 1260, 400, "right", "#55FFFF", "42px");
+  img.writeText(`&lChallenges&r &7-&r ${challenges}`, 1260, 500, "right", "#FFAA00", "42px");
+  img.writeText(`&lCoins Earned&r &7-&r ${numberify(acc.coinsEarned)}`, 1260, 600, "right", "#55FF55", "42px");
 
   const attachment = img.toDiscord();
 
