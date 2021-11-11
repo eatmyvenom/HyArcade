8d78cf35cc348a473338417727a58829bd5700ec
eatmyvenom <eat.my.venomm@gmail.com>
2021-11-10 23:40:28 -0800

    abstract leaderboard rendering

--- src/discord/images/FakeLB.js
+++ src/discord/images/FakeLB.js
@@ -1,53 +1,8 @@
 const Database = require("../Utils/Database");
-const ImageGenerator = require("./ImageGenerator");
+const Arc3 = require("./FakeLBs/Arc3");
 
-/**
- * 
- * @param {number} n
- * @returns {string} 
- */
-function formatNum (n) {
-  const r = Intl.NumberFormat("en").format(Number(n));
-  return r;
-}
 
 module.exports = async function FakeLb (path, category, time) {
-  const img = new ImageGenerator(1900, 1035, "'myFont'", false);
-  await img.addBackground("resources/lb3.png", 0, 0, 1900, 1035, "#00000000");
-
-  let y = 100;
-  const dy = 48;
-  const x = 915;
-  const fontSize = 32;
-
-  let timeTitle = path == undefined ? "Monthly" : (time == undefined) ? "Lifetime" : `${time.slice(0, 1).toUpperCase()}${time.slice(1)}`;
-  let gameTitle = "";
-
-  timeTitle += `${(path ?? "wins").slice(0, 1).toUpperCase()}${(path ?? "wins").slice(1)}`.replace(/([A-Z])/g, " $1");
-  timeTitle = timeTitle.slice(0, timeTitle.indexOf("_"));
-
-  switch(category) {
-  case "blockingDead" : gameTitle = "Blocking Dead"; break;
-  case "bountyHunters" : gameTitle = "Bounty Hunters"; break;
-  case "captureTheWool" : gameTitle = "Capture the Wool"; break;
-  case "dragonWars" : gameTitle = "Dragon Wars"; break;
-  case "enderSpleef" : gameTitle = "Ender Spleef"; break;
-  case "farmhunt" : gameTitle = "Farm Hunt"; break;
-  case "football" : gameTitle = "Football"; break;
-  case "galaxyWars" : gameTitle = "Galaxy Wars"; break;
-  case "hideAndSeek" : gameTitle = "Hide and Seek"; break;
-  case "holeInTheWall" : gameTitle = "Hole in the Wall"; break;
-  case "hypixelSays" : gameTitle = "Hypixel Says"; break;
-  case "partyGames" : gameTitle = "Party Games"; break;
-  case "pixelPainters" : gameTitle = "Pixel Painters"; break;
-  case "throwOut" : gameTitle = "Throw Out"; break;
-  case "zombies" : gameTitle = "Zombies" ; break;
-  default : gameTitle = "Mini Walls";
-  }
-
-  await img.drawNameTag(timeTitle, x, y += dy, "#55FFFF", fontSize, "'boldmc'");
-  await img.drawNameTag(gameTitle, x, y += dy, "#AAAAAA", fontSize);
-  y += 10;
 
   let topTen;
   if(path == undefined) {
@@ -56,19 +11,13 @@ module.exports = async function FakeLb (path, category, time) {
     topTen = (await Database.getLeaderboard(path, category, time)).slice(0, 10);
   }
 
-  for(let i = 0; i < topTen.length; i += 1) {
-    if(time == undefined) {
-      img.drawLBPlayer(topTen[i], `${i + 1}`, formatNum(topTen[i]?.[category]?.[path] ?? topTen[i]?.miniWalls?.wins), x, y += dy, fontSize);
-    } else {
-      img.drawLBPlayer(topTen[i], `${i + 1}`, formatNum(topTen[i]?.lbProp ?? topTen[i]?.miniWalls?.wins), x, y += dy, fontSize);
-    }
+  switch(path) {
+  case "coins": {
+    return await Arc3(path, category, time, topTen);
   }
 
-  y += 10;
-  await img.drawNameTag("Click to toggle!", x, y += dy, "#FFAA00", fontSize, "'boldmc'");
-
-  const strTime = path == undefined ? "monthly" : time ?? "lifetime";
-  await img.drawTimeType(strTime, x, y += dy, fontSize);
-
-  return img.toDiscord();
+  default : {
+    return await Arc3(path, category, time, topTen);
+  }
+  }
 };
---
+++ src/discord/images/FakeLBs/Arc3.js
@@ -0,0 +1,67 @@
+const ImageGenerator = require("./ImageGenerator");
+
+/**
+ * 
+ * @param {number} n
+ * @returns {string} 
+ */
+function formatNum (n) {
+  const r = Intl.NumberFormat("en").format(Number(n));
+  return r;
+}
+
+module.exports = async function Arc3 (path, category, time, topTen) {
+
+  const img = new ImageGenerator(1900, 1035, "'myFont'", false);
+  await img.addBackground("resources/lb3.png", 0, 0, 1900, 1035, "#00000000");
+
+  let y = 100;
+  const dy = 48;
+  const x = 915;
+  const fontSize = 32;
+
+  let timeTitle = path == undefined ? "Monthly" : (time == undefined) ? "Lifetime" : `${time.slice(0, 1).toUpperCase()}${time.slice(1)}`;
+  let gameTitle = "";
+
+  timeTitle += `${(path ?? "wins").slice(0, 1).toUpperCase()}${(path ?? "wins").slice(1)}`.replace(/([A-Z])/g, " $1");
+  timeTitle = timeTitle.slice(0, timeTitle.indexOf("_"));
+
+  switch(category) {
+  case "blockingDead" : gameTitle = "Blocking Dead"; break;
+  case "bountyHunters" : gameTitle = "Bounty Hunters"; break;
+  case "captureTheWool" : gameTitle = "Capture the Wool"; break;
+  case "dragonWars" : gameTitle = "Dragon Wars"; break;
+  case "enderSpleef" : gameTitle = "Ender Spleef"; break;
+  case "farmhunt" : gameTitle = "Farm Hunt"; break;
+  case "football" : gameTitle = "Football"; break;
+  case "galaxyWars" : gameTitle = "Galaxy Wars"; break;
+  case "hideAndSeek" : gameTitle = "Hide and Seek"; break;
+  case "holeInTheWall" : gameTitle = "Hole in the Wall"; break;
+  case "hypixelSays" : gameTitle = "Hypixel Says"; break;
+  case "partyGames" : gameTitle = "Party Games"; break;
+  case "pixelPainters" : gameTitle = "Pixel Painters"; break;
+  case "throwOut" : gameTitle = "Throw Out"; break;
+  case "zombies" : gameTitle = "Zombies" ; break;
+  default : gameTitle = "Mini Walls";
+  }
+
+  await img.drawNameTag(timeTitle, x, y += dy, "#55FFFF", fontSize, "'boldmc'");
+  await img.drawNameTag(gameTitle, x, y += dy, "#AAAAAA", fontSize);
+  y += 10;
+
+  for(let i = 0; i < topTen.length; i += 1) {
+    if(time == undefined) {
+      img.drawLBPlayer(topTen[i], `${i + 1}`, formatNum(topTen[i]?.[category]?.[path] ?? topTen[i]?.miniWalls?.wins), x, y += dy, fontSize);
+    } else {
+      img.drawLBPlayer(topTen[i], `${i + 1}`, formatNum(topTen[i]?.lbProp ?? topTen[i]?.miniWalls?.wins), x, y += dy, fontSize);
+    }
+  }
+
+  y += 10;
+  await img.drawNameTag("Click to toggle!", x, y += dy, "#FFAA00", fontSize, "'boldmc'");
+
+  const strTime = path == undefined ? "monthly" : time ?? "lifetime";
+  await img.drawTimeType(strTime, x, y += dy, fontSize);
+
+  return img.toDiscord();
+};
