0247d07066ade6c88e6f787bdfa315f399a2632d
eatmyvenom <eat.my.venomm@gmail.com>
2021-11-22 23:29:40 -0800

    cycle presense caps

--- src/discord/Commands/CyclePresence.mjs
+++ src/discord/Commands/CyclePresence.mjs
@@ -2,7 +2,7 @@ import Command from "../../classes/Command.js";
 import BotRuntime from "../BotRuntime.js";
 import SetPresence from "../Utils/SetPresence.js";
 
-export default new Command("CyclePresence", ["*"], async () => {
+export default new Command("cyclepresence", ["*"], async () => {
   await SetPresence(BotRuntime.client, BotRuntime.botMode);
   return {
     res: "Presence cycled"
