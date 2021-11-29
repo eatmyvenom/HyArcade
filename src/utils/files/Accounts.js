85eb02f8fbc91d17f21ef5cbc9b433b094377436
eatmyvenom <eat.my.venomm@gmail.com>
2021-11-28 20:16:57 -0800

    add system to store each account as seperate file

---
+++ src/utils/files/Accounts.js
@@ -0,0 +1,57 @@
+const fs = require("fs-extra");
+const Account = require("hyarcade-requests/types/Account");
+
+module.exports = class Accounts {
+  folderPath = "";
+
+  constructor (path) {
+    this.folderPath = path;
+  }
+
+  /**
+   *
+   *
+   * @param {string} uuid
+   * @returns {Account} 
+   */
+  async readAccount (uuid) {
+    const realUuid = uuid.includes(".json") ? uuid : `${uuid}.json`;
+
+    const file = await fs.readFile(`${this.folderPath}/${realUuid}`);
+    const data = JSON.parse(file);
+
+    return Account.from(data);
+  }
+
+  /**
+   *
+   *
+   * @param {string} uuid
+   * @param {Account} data
+   */
+  async writeAccount (uuid, data) {
+    const string = JSON.stringify(data);
+
+    await fs.writeFile(`${this.folderPath}/${uuid ?? data.uuid}.json`, string);
+  }
+
+  /**
+   * 
+   * @param {Account[]} accounts 
+   */
+  async writeAccounts (accounts) {
+    for(const account of accounts) {
+      await this.writeAccount(account.uuid, account);
+    }
+  }
+
+  async readAccounts () {
+    const files = await fs.readdir(this.folderPath);
+
+    const accounts = [];
+
+    for(const fileName of files) {
+      accounts.push(await this.readAccount(fileName));
+    }
+  }
+};
