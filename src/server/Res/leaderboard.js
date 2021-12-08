ba100fd72b83f2a832dbfc3bf01bda72aa9de895
eatmyvenom <eat.my.venomm@gmail.com>
2021-12-07 20:14:44 -0800

    fix lbs v2

--- src/server/Res/leaderboard.js
+++ src/server/Res/leaderboard.js
@@ -72,7 +72,9 @@ module.exports = async (req, res, fileCache) => {
     Logger.verbose("Sorting accounts");
     if(timePeriod == undefined) {
       TimSort.sort(accs, (b, a) => numberify(getter(a)) - numberify(getter(b)));
-      accs = accs.reverse();
+      if(reverse) {
+        accs = accs.reverse();
+      }
     } else {
       const newAccs = [];
       const old = fileCache[`indexed${timePeriod}`];
