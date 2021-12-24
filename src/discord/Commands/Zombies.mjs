f37df8af075edc016720b296709b8377324632e8
eatmyvenom <eat.my.venomm@gmail.com>
2021-12-24 01:40:11 -0800

    readd delimiter for mobile formatting

--- src/discord/Commands/Zombies.mjs
+++ src/discord/Commands/Zombies.mjs
@@ -58,22 +58,22 @@ function createDefaultEmbed (acc) {
   return new MessageEmbed()
     .setTitle(`${acc.name} Zombies stats`)
     .addField("Stats",
-      `・**Wins** \`${numberify(acc.zombies?.wins_zombies ?? 0)}\`\n` +
-      `・**Rounds** \`${numberify(acc.zombies?.total_rounds_survived_zombies ?? 0)}\`\n` +
-      `・**Kills** \`${numberify(acc.zombies?.zombie_kills_zombies ?? 0)}\`\n` +
-      `・**Knocks** \`${numberify(acc.zombies?.times_knocked_down_zombies ?? 0)}\`\n` +
-      `・**Deaths** \`${numberify(acc.zombies?.deaths_zombies ?? 0)}\`\n`,
+      `・**Wins** - \`${numberify(acc.zombies?.wins_zombies ?? 0)}\`\n` +
+      `・**Rounds** - \`${numberify(acc.zombies?.total_rounds_survived_zombies ?? 0)}\`\n` +
+      `・**Kills** - \`${numberify(acc.zombies?.zombie_kills_zombies ?? 0)}\`\n` +
+      `・**Knocks** - \`${numberify(acc.zombies?.times_knocked_down_zombies ?? 0)}\`\n` +
+      `・**Deaths** - \`${numberify(acc.zombies?.deaths_zombies ?? 0)}\`\n`,
       true)
     .addField("Info",
-      `・**Fastest win** \`${toHHMMSS(Math.min(acc.zombies?.fastest_time_30_zombies_badblood_normal ?? 99999, acc.zombies?.fastest_time_30_zombies_deadend_normal ?? 99999, acc.zombies?.fastest_time_30_zombies_alienarcadium_normal ?? 99999))}\`\n` +
-      `・**Doors opened** \`${numberify(acc.zombies?.doors_opened_zombies ?? 0)}\`\n` +
-      `・**Revives** \`${numberify(acc.zombies?.players_revived_zombies ?? 0)}\`\n`,
+      `・**Fastest win** - \`${toHHMMSS(Math.min(acc.zombies?.fastest_time_30_zombies_badblood_normal ?? 99999, acc.zombies?.fastest_time_30_zombies_deadend_normal ?? 99999, acc.zombies?.fastest_time_30_zombies_alienarcadium_normal ?? 99999))}\`\n` +
+      `・**Doors opened** - \`${numberify(acc.zombies?.doors_opened_zombies ?? 0)}\`\n` +
+      `・**Revives** - \`${numberify(acc.zombies?.players_revived_zombies ?? 0)}\`\n`,
       true)
     .addField("Ratios",
-      `・**Kills/Rounds** \`${numberify((acc.zombies?.zombie_kills_zombies ?? 0) / (acc.zombies?.total_rounds_survived_zombies ?? 0))}\`\n` +
-      `・**Revives/Deaths** \`${numberify((acc.zombies?.players_revived_zombies ?? 0) / (acc.zombies?.deaths_zombies ?? 0))}\`\n` +
-      `・**Accuracy** \`${percentify((acc.zombies?.bullets_hit_zombies ?? 0) / (acc.zombies?.bullets_shot_zombies ?? 0))}\`\n` +
-      `・**Headshots** \`${percentify((acc.zombies?.headshots_zombies ?? 0) / (acc.zombies?.bullets_hit_zombies ?? 0))}\``,
+      `・**Kills/Rounds** - \`${numberify((acc.zombies?.zombie_kills_zombies ?? 0) / (acc.zombies?.total_rounds_survived_zombies ?? 0))}\`\n` +
+      `・**Revives/Deaths** - \`${numberify((acc.zombies?.players_revived_zombies ?? 0) / (acc.zombies?.deaths_zombies ?? 0))}\`\n` +
+      `・**Accuracy** - \`${percentify((acc.zombies?.bullets_hit_zombies ?? 0) / (acc.zombies?.bullets_shot_zombies ?? 0))}\`\n` +
+      `・**Headshots** - \`${percentify((acc.zombies?.headshots_zombies ?? 0) / (acc.zombies?.bullets_hit_zombies ?? 0))}\``,
       false)
     .setThumbnail(`https://crafatar.com/renders/head/${acc.uuid}?overlay`)
     .setColor(0x44a3e7);
@@ -88,21 +88,21 @@ function createBadBloodEmbed (acc) {
   return new MessageEmbed()
     .setTitle(`${acc.name} Bad Blood stats`)
     .addField("Stats",
-      `・**Wins** \`${numberify(acc.zombies?.wins_zombies_badblood ?? 0)}\`\n` +
-        `・**Rounds** \`${numberify(acc.zombies?.total_rounds_survived_zombies_badblood ?? 0)}\`\n` +
-        `・**Kills** \`${numberify(acc.zombies?.zombie_kills_zombies_badblood ?? 0)}\`\n` +
-        `・**Knocks** \`${numberify(acc.zombies?.times_knocked_down_zombies_badblood ?? 0)}\`\n` +
-        `・**Deaths** \`${numberify(acc.zombies?.deaths_zombies_badblood ?? 0)}\`\n`,
+      `・**Wins** - \`${numberify(acc.zombies?.wins_zombies_badblood ?? 0)}\`\n` +
+        `・**Rounds** - \`${numberify(acc.zombies?.total_rounds_survived_zombies_badblood ?? 0)}\`\n` +
+        `・**Kills** - \`${numberify(acc.zombies?.zombie_kills_zombies_badblood ?? 0)}\`\n` +
+        `・**Knocks** - \`${numberify(acc.zombies?.times_knocked_down_zombies_badblood ?? 0)}\`\n` +
+        `・**Deaths** - \`${numberify(acc.zombies?.deaths_zombies_badblood ?? 0)}\`\n`,
       true)
     .addField("Info",
-      `・**Doors opened** \`${numberify(acc.zombies?.doors_opened_zombies_badblood ?? 0)}\`\n` +
-        `・**Fastest win** \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_badblood_normal ?? 99999)}\`\n` +
-        `・**Best round** \`${numberify(acc.zombies?.best_round_zombies_badblood ?? 0)}\`\n` +
-        `・**Revives** \`${numberify(acc.zombies?.players_revived_zombies_badblood ?? 0)}\`\n`,
+      `・**Doors opened** - \`${numberify(acc.zombies?.doors_opened_zombies_badblood ?? 0)}\`\n` +
+        `・**Fastest win** - \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_badblood_normal ?? 99999)}\`\n` +
+        `・**Best round** - \`${numberify(acc.zombies?.best_round_zombies_badblood ?? 0)}\`\n` +
+        `・**Revives** - \`${numberify(acc.zombies?.players_revived_zombies_badblood ?? 0)}\`\n`,
       true)
     .addField("Ratios",
-      `・**Kills/Rounds** \`${numberify((acc.zombies?.zombie_kills_zombies_badblood ?? 0) / (acc.zombies?.total_rounds_survived_zombies_badblood ?? 0))}\`\n` +
-        `・**Revives/Deaths** \`${numberify((acc.zombies?.players_revived_zombies_badblood ?? 0) / (acc.zombies?.deaths_zombies_badblood ?? 0))}\``,
+      `・**Kills/Rounds** - \`${numberify((acc.zombies?.zombie_kills_zombies_badblood ?? 0) / (acc.zombies?.total_rounds_survived_zombies_badblood ?? 0))}\`\n` +
+        `・**Revives/Deaths** - \`${numberify((acc.zombies?.players_revived_zombies_badblood ?? 0) / (acc.zombies?.deaths_zombies_badblood ?? 0))}\``,
       false)
     .setThumbnail(`https://crafatar.com/renders/head/${acc.uuid}?overlay`)
     .setColor(0x44a3e7);
@@ -117,21 +117,21 @@ function createDeadEndEmbed (acc) {
   return new MessageEmbed()
     .setTitle(`${acc.name} Dead End stats`)
     .addField("Stats",
-      `・**Wins** \`${numberify(acc.zombies?.wins_zombies_deadend ?? 0)}\`\n` +
-        `・**Rounds** \`${numberify(acc.zombies?.total_rounds_survived_zombies_deadend ?? 0)}\`\n` +
-        `・**Kills** \`${numberify(acc.zombies?.zombie_kills_zombies_deadend ?? 0)}\`\n` +
-        `・**Knocks** \`${numberify(acc.zombies?.times_knocked_down_zombies_deadend ?? 0)}\`\n` +
-        `・**Deaths** \`${numberify(acc.zombies?.deaths_zombies_deadend ?? 0)}\`\n`,
+      `・**Wins** - \`${numberify(acc.zombies?.wins_zombies_deadend ?? 0)}\`\n` +
+        `・**Rounds** - \`${numberify(acc.zombies?.total_rounds_survived_zombies_deadend ?? 0)}\`\n` +
+        `・**Kills** - \`${numberify(acc.zombies?.zombie_kills_zombies_deadend ?? 0)}\`\n` +
+        `・**Knocks** - \`${numberify(acc.zombies?.times_knocked_down_zombies_deadend ?? 0)}\`\n` +
+        `・**Deaths** - \`${numberify(acc.zombies?.deaths_zombies_deadend ?? 0)}\`\n`,
       true)
     .addField("Info",
-      `・**Doors opened** \`${numberify(acc.zombies?.doors_opened_zombies_deadend ?? 0)}\`\n` +
-        `・**Fastest win** \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_deadend_normal ?? 99999)}\`\n` +
-        `・**Best round** \`${numberify(acc.zombies?.best_round_zombies_deadend ?? 0)}\`\n` +
-        `・**Revives** \`${numberify(acc.zombies?.players_revived_zombies_deadend ?? 0)}\`\n`,
+      `・**Doors opened** - \`${numberify(acc.zombies?.doors_opened_zombies_deadend ?? 0)}\`\n` +
+        `・**Fastest win** - \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_deadend_normal ?? 99999)}\`\n` +
+        `・**Best round** - \`${numberify(acc.zombies?.best_round_zombies_deadend ?? 0)}\`\n` +
+        `・**Revives** - \`${numberify(acc.zombies?.players_revived_zombies_deadend ?? 0)}\`\n`,
       true)
     .addField("Ratios",
-      `・**Kills/Rounds** \`${numberify((acc.zombies?.zombie_kills_zombies_deadend ?? 0) / (acc.zombies?.total_rounds_survived_zombies_deadend ?? 0))}\`\n` +
-        `・**Revives/Deaths** \`${numberify((acc.zombies?.players_revived_zombies_deadend ?? 0) / (acc.zombies?.deaths_zombies_deadend ?? 0))}\``,
+      `・**Kills/Rounds** - \`${numberify((acc.zombies?.zombie_kills_zombies_deadend ?? 0) / (acc.zombies?.total_rounds_survived_zombies_deadend ?? 0))}\`\n` +
+        `・**Revives/Deaths** - \`${numberify((acc.zombies?.players_revived_zombies_deadend ?? 0) / (acc.zombies?.deaths_zombies_deadend ?? 0))}\``,
       false)
     .setThumbnail(`https://crafatar.com/renders/head/${acc.uuid}?overlay`)
     .setColor(0x44a3e7);
@@ -146,21 +146,21 @@ function createAlienArcadiumEmbed (acc) {
   return new MessageEmbed()
     .setTitle(`${acc.name} Alien Arcadium stats`)
     .addField("Stats",
-      `・**Wins** \`${numberify(acc.zombies?.wins_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Rounds** \`${numberify(acc.zombies?.total_rounds_survived_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Kills** \`${numberify(acc.zombies?.zombie_kills_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Knocks** \`${numberify(acc.zombies?.times_knocked_down_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Deaths** \`${numberify(acc.zombies?.deaths_zombies_alienarcadium ?? 0)}\`\n`,
+      `・**Wins** - \`${numberify(acc.zombies?.wins_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Rounds** - \`${numberify(acc.zombies?.total_rounds_survived_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Kills** - \`${numberify(acc.zombies?.zombie_kills_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Knocks** - \`${numberify(acc.zombies?.times_knocked_down_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Deaths** - \`${numberify(acc.zombies?.deaths_zombies_alienarcadium ?? 0)}\`\n`,
       true)
     .addField("Info",
-      `・**Doors opened** \`${numberify(acc.zombies?.doors_opened_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Fastest win** \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_alienarcadium_normal ?? 99999)}\`\n` +
-        `・**Best round** \`${numberify(acc.zombies?.best_round_zombies_alienarcadium ?? 0)}\`\n` +
-        `・**Revives** \`${numberify(acc.zombies?.players_revived_zombies_alienarcadium ?? 0)}\`\n`,
+      `・**Doors opened** - \`${numberify(acc.zombies?.doors_opened_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Fastest win** - \`${toHHMMSS(acc.zombies?.fastest_time_30_zombies_alienarcadium_normal ?? 99999)}\`\n` +
+        `・**Best round** - \`${numberify(acc.zombies?.best_round_zombies_alienarcadium ?? 0)}\`\n` +
+        `・**Revives** - \`${numberify(acc.zombies?.players_revived_zombies_alienarcadium ?? 0)}\`\n`,
       true)
     .addField("Ratios",
-      `・**Kills/Rounds** \`${numberify((acc.zombies?.zombie_kills_zombies_alienarcadium ?? 0) / (acc.zombies?.total_rounds_survived_zombies_alienarcadium ?? 0))}\`\n` +
-        `・**Revives/Deaths** \`${numberify((acc.zombies?.players_revived_zombies_alienarcadium ?? 0) / (acc.zombies?.deaths_zombies_alienarcadium ?? 0))}\``,
+      `・**Kills/Rounds** - \`${numberify((acc.zombies?.zombie_kills_zombies_alienarcadium ?? 0) / (acc.zombies?.total_rounds_survived_zombies_alienarcadium ?? 0))}\`\n` +
+        `・**Revives/Deaths** - \`${numberify((acc.zombies?.players_revived_zombies_alienarcadium ?? 0) / (acc.zombies?.deaths_zombies_alienarcadium ?? 0))}\``,
       false)
     .setThumbnail(`https://crafatar.com/renders/head/${acc.uuid}?overlay`)
     .setColor(0x44a3e7);
