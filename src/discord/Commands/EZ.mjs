bc56e31d57a59eddfaaafe3e6499a6ca00945bfb
eatmyvenom <eat.my.venomm@gmail.com>
2021-12-27 21:24:05 -0800

    cache ezmsgs

--- src/discord/Commands/EZ.mjs
+++ src/discord/Commands/EZ.mjs
@@ -2,8 +2,13 @@ import Command from "../../classes/Command.js";
 import BotRuntime from "../BotRuntime.js";
 import CommandResponse from "../Utils/CommandResponse.js";
 
+let msgs = undefined;
+
 export default new Command("ez", ["*"], async () => {
-  const msgs = await BotRuntime.getFromDB("ezmsgs");
+  if(msgs == undefined) {
+    msgs = await BotRuntime.getFromDB("ezmsgs");
+  }
+
   let msg = msgs[Math.floor(Math.random() * msgs.length)];
   msg = msg.replace(/\\n/g, "\n");
   return new CommandResponse(msg);
