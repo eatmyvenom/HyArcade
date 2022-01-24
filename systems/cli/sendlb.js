730506fffc14e472f3ad92696a97e269e5dc1df8
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-23 21:07:14 -0800

    fix(discord): :speech_balloon: work around spaces in titles
    adds a workaround for spaces in the titles of "sendlb" commands

--- systems/cli/sendlb.js
+++ systems/cli/sendlb.js
@@ -9,9 +9,9 @@ const config = require("hyarcade-config").fromJSON();
  */
 async function main (args) {
   if(args[3] == "basic") {
-    return await SendBasicLB(config.otherHooks[args[4] ?? "TEST"], args[5] ?? "arcadeWins", args[6], args[7] ?? 10, args[8] ?? "title");
+    return await SendBasicLB(config.otherHooks[args[4] ?? "TEST"], args[5] ?? "arcadeWins", args[6], args[7] ?? 10, (args[8] ?? "title").replace(/_/g, " "));
   } else if(args[3] == "duplex") {
-    return await SendDuplexLB(config.otherHooks[args[4] ?? "TEST"], args[5] ?? "arcadeWins", args[6], args[7] ?? 10, args[8] ?? "title", args[9] ?? "day");
+    return await SendDuplexLB(config.otherHooks[args[4] ?? "TEST"], args[5] ?? "arcadeWins", args[6], args[7] ?? 10, (args[8] ?? "title").replace(/_/g, " "), args[9] ?? "day");
   }
 }
 
