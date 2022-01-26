a3b208e80e59890aa3ca95f951aa49c577f6e949
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-25 22:46:10 -0800

    fix(mongo): :card_file_box: use correct sort direction

--- tests/MongoDb.js
+++ tests/MongoDb.js
@@ -1,5 +1,4 @@
 const Logger = require("hyarcade-logger");
-const Database = require("hyarcade-requests/Database");
 const connector = require("hyarcade-requests/MongoConnector");
 
 /**
@@ -8,9 +7,6 @@ const connector = require("hyarcade-requests/MongoConnector");
 async function main() {
   const c = new connector("mongodb://127.0.0.1:27017");
   await c.connect();
-
-  const accs = await Database.readDB("accounts");
-  await c.updateAccounts(accs);
 
   const lb = await c.getLeaderboard("partyGames.wins", false, false, 10, false);
   console.dir(lb);
