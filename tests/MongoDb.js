b534f5b1afe0f6a33894e5afec28ab6bf82303b8
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-28 21:29:27 -0800

    feat(mongo): :card_file_box: update test

--- tests/MongoDb.js
+++ tests/MongoDb.js
@@ -1,4 +1,5 @@
 const Logger = require("hyarcade-logger");
+const Database = require("hyarcade-requests/Database");
 const connector = require("hyarcade-requests/MongoConnector");
 
 /**
@@ -8,8 +9,14 @@ async function main() {
   const c = new connector("mongodb://127.0.0.1:27017");
   await c.connect();
 
-  const lb = await c.getLeaderboard("partyGames.wins", false, false, 10, false);
-  console.dir(lb);
+  const dayAccs = await Database.readDB("dayaccounts");
+  await c.updateDaily(dayAccs);
+  
+  const weekAccs = await Database.readDB("weeklyaccounts");
+  await c.updateWeekly(weekAccs);
+
+  const monthAccs = await Database.readDB("monthlyaccounts");
+  await c.updateMonthly(monthAccs);
 
   await c.destroy();
 }
