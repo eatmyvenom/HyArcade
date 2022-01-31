31245b9ef2606cc2d1db59026cdeb9262430c7c5
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-30 22:39:59 -0800

    feat(cli): :sparkles: make full tests for mongodb

--- tests/MongoDb.js
+++ tests/MongoDb.js
@@ -1,14 +1,37 @@
 const Logger = require("hyarcade-logger");
 const connector = require("hyarcade-requests/MongoConnector");
+const Sleep = require("hyarcade-utils/Sleep");
 
 /**
  *
  */
 async function main() {
   const c = new connector("mongodb://127.0.0.1:27017");
-  await c.connect();
+  await c.connect(false);
 
-  Logger.out(JSON.stringify(await c.getHistoricalLeaderboard("partyGames.starsEarned", "weekly", false, 10), null, 2));
+  Logger.out("partyGames.wins");
+  Logger.out(JSON.stringify(await c.getLeaderboard("partyGames.starsEarned", false, 2), null, 2));
+  Logger.out("");
+  await Sleep(5000);
+
+  Logger.info("partyGames.starsEarned weekly");
+  Logger.out(JSON.stringify(await c.getHistoricalLeaderboard("partyGames.starsEarned", "weekly", false, 2), null, 2));
+  Logger.out("");
+  await Sleep(5000);
+
+  Logger.info("vnmm acc - ign");
+  Logger.out(JSON.stringify(await c.getAccount("vnmm")));
+  Logger.out("");
+  await Sleep(5000);
+
+  Logger.info("vnmm acc - uuid");
+  Logger.out(JSON.stringify(await c.getAccount("92a5199614ac4bd181d1f3c951fb719f")));
+  Logger.out("");
+  await Sleep(5000);
+
+  Logger.info("vnmm acc - discord");
+  Logger.out(JSON.stringify(await c.getAccount("156952208045375488")));
+  Logger.out("");
 
   await c.destroy();
 }
