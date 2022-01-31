cad6c18009b6f0407c9a97afb8cac25b948831e6
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-30 22:59:16 -0800

    fix(mongo): :card_file_box: typo

--- systems/cli/exporttomongo.js
+++ systems/cli/exporttomongo.js
@@ -9,22 +9,6 @@ async function main() {
   const connector = new MongoConnector("mongodb://127.0.0.1:27017");
   await connector.connect(false);
 
-  const accounts = await Database.readDB("accounts");
-  await connector.updateAccounts(accounts);
-  Logger.out("Updated accounts");
-
-  const dayAccounts = await Database.readDB("dayaccounts");
-  await connector.updateDaily(dayAccounts);
-  Logger.out("Updated daily");
-
-  const weekAccounts = await Database.readDB("weeklyaccounts");
-  await connector.updateWeekly(weekAccounts);
-  Logger.out("Updated weekly");
-
-  const monthlyAccounts = await Database.readDB("monthlyaccounts");
-  await connector.updateMonthly(monthlyAccounts);
-  Logger.out("Updated monthly");
-
   const disclist = await Database.readDB("disclist");
   for(const link in disclist) {
     await connector.linkDiscord(link, disclist[link]);
