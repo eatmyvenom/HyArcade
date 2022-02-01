7ec8ab5956879b83069168e204a30593294bf2f3
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-31 21:45:25 -0800

    fix(discord): :speech_balloon: fix fakelb

--- systems/discord/images/FakeLB.js
+++ systems/discord/images/FakeLB.js
@@ -7,15 +7,17 @@ const Guild = require("./FakeLBs/Guild");
 
 module.exports = async function FakeLb (path, category, time) {
 
+  let realTime = time;
   let topTen;
 
   if(category == "guild") {
     topTen = await getFromDB("guilds");
     topTen = topTen.sort((a, b) => b?.[path] - a?.[path]);
   } else if(path == undefined) {
+    realTime = "monthly";
     topTen = await Database.getMWLeaderboard("wins", "monthly");
   } else {
-    topTen = await Database.getLeaderboard(path, category, time);
+    topTen = await Database.getLeaderboard(path, category, realTime);
   }
 
   const banlist = await getBanlist();
@@ -25,17 +27,17 @@ module.exports = async function FakeLb (path, category, time) {
   switch(path) {
   case "coinsEarned":
   case "arcadeCoins": {
-    return await ArcLeft(path, undefined, time, topTen);
+    return await ArcLeft(path, undefined, realTime, topTen);
   }
 
   default : {
     switch (category) {
     case "guild": {
-      return Guild(path, category, time, topTen);
+      return Guild(path, category, realTime, topTen);
     }
 
     default: {
-      return await Arc3(path, category, time, topTen);
+      return await Arc3(path, category, realTime, topTen);
     }
     }
   }
