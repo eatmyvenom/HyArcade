d818b8103ee9c6520809c514e01cdf260478823a
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-01 17:57:19 -0800

    feat(mongo): :card_file_box: update mongo tests

--- tests/MongoDb.js
+++ tests/MongoDb.js
@@ -10,7 +10,7 @@ async function main() {
   await c.connect(false);
 
   Logger.out("partyGames.wins");
-  Logger.out(JSON.stringify(await c.getLeaderboard("partyGames.starsEarned", false, 2), null, 2));
+  Logger.out(JSON.stringify(await c.getLeaderboard("partyGames.wins", false, 2), null, 2));
   Logger.out("");
   await Sleep(5000);
 
@@ -20,17 +20,17 @@ async function main() {
   await Sleep(5000);
 
   Logger.info("vnmm acc - ign");
-  Logger.out(JSON.stringify(await c.getAccount("vnmm")));
+  Logger.out(JSON.stringify(await c.getAccount("vnmm"), [ "wins", "name", "uuid"]));
   Logger.out("");
   await Sleep(5000);
 
   Logger.info("vnmm acc - uuid");
-  Logger.out(JSON.stringify(await c.getAccount("92a5199614ac4bd181d1f3c951fb719f")));
+  Logger.out(JSON.stringify(await c.getAccount("92a5199614ac4bd181d1f3c951fb719f"), [ "wins", "name", "uuid"]));
   Logger.out("");
   await Sleep(5000);
 
   Logger.info("vnmm acc - discord");
-  Logger.out(JSON.stringify(await c.getAccount("156952208045375488")));
+  Logger.out(JSON.stringify(await c.getAccount("156952208045375488"), [ "wins", "name", "uuid"]));
   Logger.out("");
 
   await c.destroy();
