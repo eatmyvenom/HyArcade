070fd0c1ad89975da5a1bbcb30932920e109cda2
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-02 23:57:38 -0800

    fix(api): :ambulance: fix dot notation

--- src/utils/leaderboard/GenericLeaderboard.js
+++ src/utils/leaderboard/GenericLeaderboard.js
@@ -15,7 +15,7 @@ const MongoConnector = require("hyarcade-requests/MongoConnector");
  */
 module.exports = async function (category, lbprop, timePeriod, reverse, max, filter = false, connector) {
   Logger.verbose("Getting leaderboard");
-  const dotNotated = `${category}.${lbprop}`.replace(/^\./g, "").replace(/\.*/g, ".");
+  const dotNotated = `${category}.${lbprop}`.replace(/^\./, "").replace(/\.\./g, ".");
 
   let realFilter = false;
   if (filter && !Array.isArray(filter)) {
