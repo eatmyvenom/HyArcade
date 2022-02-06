67b4be835ad8cfb9c5ecb8209ea1d4a9d4e17ffd
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-05 22:12:21 -0800

    feat(discord): :speech_balloon: allow for async eval

--- systems/discord/Commands/Mongo.mjs
+++ systems/discord/Commands/Mongo.mjs
@@ -3,12 +3,15 @@ import Command from "hyarcade-structures/Discord/Command.js";
 import CommandResponse from "hyarcade-structures/Discord/CommandResponse.js";
 import { inspect } from "node:util";
 
+// eslint-disable-next-line prefer-arrow-callback
+const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+
 /**
  * @param {string} str
  * @returns {string}
  */
 function safeEval(str) {
-  return new Function("c", `"use strict";return (${str})`);
+  return new AsyncFunction("c", `"use strict";return (${str})`);
 }
 
 export default new Command("mongoeval", ["156952208045375488"], async args => {
@@ -17,7 +20,7 @@ export default new Command("mongoeval", ["156952208045375488"], async args => {
   const connector = new MongoConnector("mongodb://127.0.0.1:27017");
   await connector.connect(false);
 
-  let evaled = f(connector);
+  let evaled = await f(connector);
 
   if (typeof evaled != "string") {
     evaled = inspect(evaled, true);
