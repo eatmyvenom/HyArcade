41f6acef234ee9d6e27362eca7e2931d51ca9811
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-06 17:29:57 -0800

    feat(api): :zap: update guild data if its under 2 hours old

--- systems/server/endpoints/guild.js
+++ systems/server/endpoints/guild.js
@@ -27,7 +27,7 @@ module.exports = async (req, res, connector) => {
     }
 
     // eslint-disable-next-line unicorn/no-null
-    if (guild == undefined || guild == null) {
+    if (guild == undefined || guild == null || (guild.updateTime ?? 0) < Date.now() - 14400000) {
       guild = new Guild(uuid ?? memberUUID);
       await guild.updateWins();
 
