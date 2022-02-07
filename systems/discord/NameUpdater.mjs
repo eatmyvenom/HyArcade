c3a1102ce7e645efa06d2df9a989c84092f58f40
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-06 23:21:54 -0800

    feat(discord): :speech_balloon: update names using disclist

--- systems/discord/NameUpdater.mjs
+++ systems/discord/NameUpdater.mjs
@@ -1,8 +1,7 @@
 import logger from "hyarcade-logger";
 import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-
 import BotRuntime from "./BotRuntime.js";
+const require = createRequire(import.meta.url);
 
 const { Client } = require("discord.js");
 
@@ -11,18 +10,27 @@ const { Client } = require("discord.js");
  */
 export default async function NameUpdater(client) {
   logger.info("Updating usernames in mini walls discord");
-  const accs = await BotRuntime.getFromDB("accounts", ["uuid", "name", "discord", "hypixelDiscord"]);
+  const accs = await BotRuntime.getFromDB("accounts", ["uuid", "name", "hypixelDiscord"]);
+  const list = await BotRuntime.getFromDB("discordList");
+  const disclist = {};
+
+  for (const link of list) {
+    disclist[link.discordID] = link.uuid;
+  }
 
   const mwServer = await client.guilds.fetch("789718245015289886");
   const mwMembers = await mwServer.members.fetch();
 
   for (const m of mwMembers) {
-    const acc = accs.find(a => a.discord == m.id);
-    if (acc != undefined && acc.name != undefined && acc.name != "INVALID-NAME" && acc.name != m.displayName) {
-      try {
-        await m.setNickname(acc.name);
-      } catch (error) {
-        logger.err(error.stack);
+    const uuid = disclist[m.id];
+    if (uuid != undefined) {
+      const acc = accs.find(a => a.uuid == uuid);
+      if (acc != undefined && acc.name != undefined && acc.name != "INVALID-NAME" && acc.name != m.displayName) {
+        try {
+          await m.setNickname(acc.name);
+        } catch (error) {
+          logger.err(error.stack);
+        }
       }
     }
   }
