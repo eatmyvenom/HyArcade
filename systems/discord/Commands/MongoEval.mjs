391588eb28b6f75d05f08b85a0093edd63c9668e
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-17 12:44:48 -0800

    chore(discord): :speech_balloon: make mongoeval use internal endpoint

---
+++ systems/discord/Commands/MongoEval.mjs
@@ -0,0 +1,15 @@
+import Database from "hyarcade-requests/Database";
+import Command from "hyarcade-structures/Discord/Command.js";
+import CommandResponse from "hyarcade-structures/Discord/CommandResponse.js";
+import { inspect } from "node:util";
+
+export default new Command("mongoeval", ["156952208045375488"], async args => {
+  let evaled = Database.internal({ mongoEval: args.join(" ") });
+
+  if (typeof evaled != "string") {
+    evaled = inspect(evaled, true);
+  }
+
+  const res = `\`\`\`\nResponse:\n${evaled}\n\`\`\``;
+  return new CommandResponse(res);
+});
