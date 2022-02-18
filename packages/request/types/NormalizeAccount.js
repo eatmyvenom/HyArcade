0fcb2427332acba06dad2f197d921f655c587cdb
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-18 13:34:29 -0800

    chore(datagen): :chart_with_upwards_trend: adjust account normalizer

--- packages/request/types/NormalizeAccount.js
+++ packages/request/types/NormalizeAccount.js
@@ -2,19 +2,19 @@ module.exports = function NormalizeAccount(acc) {
   return (
     acc.blockingDead.wins * 7 +
     acc.bountyHunters.wins * 8 +
-    acc.dragonWars.wins * 11 +
-    acc.enderSpleef.wins * 4.5 +
-    acc.farmhunt.wins * 6 +
+    acc.dragonWars.wins * 10 +
+    acc.enderSpleef.wins * 5 +
+    acc.farmhunt.wins * 5 +
     acc.football.wins * 2 +
     acc.galaxyWars.wins * 7 +
     acc.miniWalls.wins * 3 +
     acc.hideAndSeek.wins * 3 +
-    acc.hypixelSays.wins * 2.5 +
-    acc.partyGames.wins * 7.5 +
-    acc.pixelPainters.wins * 10 +
-    acc.holeInTheWall.wins * 5 +
-    acc.throwOut.wins * 8 +
-    acc.seasonalWins.total * 4 +
+    acc.hypixelSays.wins * 3 +
+    acc.partyGames.wins * 7 +
+    acc.pixelPainters.wins * 11 +
+    acc.holeInTheWall.wins * 6 +
+    acc.throwOut.wins * 9 +
+    acc.seasonalWins.total * 3 +
     (acc.zombies.wins_zombies ?? 0) * 30
   );
 };
