9247b966667c6024322d3e71c663e28fa79769ef
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-23 00:44:35 -0800

    feat(api): :zap: add other relavent stats

--- systems/server/endpoints/Status.js
+++ systems/server/endpoints/Status.js
@@ -23,6 +23,9 @@ module.exports = async (req, res, connector) => {
     status.rank = resolvedAccount.rank;
     status.mvpColor = resolvedAccount.mvpColor;
     status.plusColor = resolvedAccount.plusColor;
+    status.lastLogout = resolvedAccount.lastLogout;
+    status.lastLogin = resolvedAccount.lastLogin;
+    status.isLoggedIn = resolvedAccount.isLoggedIn;
 
     if (cacheAccount.extra.combinedAchievementProgress != resolvedAccount.extra.combinedAchievementProgress) {
       status.actionTime.otherActions = Math.max(status.actionTime.otherActions, cacheAccount.updateTime);
