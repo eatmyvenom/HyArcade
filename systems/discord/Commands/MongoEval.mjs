f6cf5563810f3710b649cad7d0a0683295711459
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-24 16:11:33 -0800

    fix(discord): :speech_balloon: missing await

--- systems/discord/Commands/MongoEval.mjs
+++ systems/discord/Commands/MongoEval.mjs
@@ -4,7 +4,7 @@ import CommandResponse from "hyarcade-structures/Discord/CommandResponse.js";
 import { inspect } from "node:util";
 
 export default new Command("mongoeval", ["156952208045375488"], async args => {
-  let evaled = Database.internal({ mongoEval: args.join(" ") });
+  let evaled = await Database.internal({ mongoEval: args.join(" ") });
 
   if (typeof evaled != "string") {
     evaled = inspect(evaled, true);
