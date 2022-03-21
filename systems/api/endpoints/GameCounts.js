7ea3239874cb7a56353480b0d9a1a9e4a7531a76
eatmyvenom <eat.my.venomm@gmail.com>
2022-03-20 23:09:58 -0700

    fix(api): :zap: correctly get game counts

--- systems/api/endpoints/GameCounts.js
+++ systems/api/endpoints/GameCounts.js
@@ -14,11 +14,11 @@ module.exports = async (req, res, connector, redisInterface) => {
     res.setHeader("Content-Type", "application/json");
 
     let counts;
-    if (!redisInterface.exists("counts")) {
+    if (await redisInterface.exists("counts")) {
+      counts = redisInterface.getJSON(counts);
+    } else {
       counts = await HypixelApi.counts();
       await redisInterface.setJSON("counts", counts, 600);
-    } else {
-      counts = redisInterface.getJSON(counts);
     }
 
     res.write(JSON.stringify(counts));
