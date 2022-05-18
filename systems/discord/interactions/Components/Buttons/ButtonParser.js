b29e64b223f5f494b835a7c444e7690b5d4c2e23
eatmyvenom <eat.my.venomm@gmail.com>
2022-05-18 01:21:49 -0400

    fix(buttons): stats command

--- systems/discord/interactions/Components/Buttons/ButtonParser.js
+++ systems/discord/interactions/Components/Buttons/ButtonParser.js
@@ -21,7 +21,7 @@ async function statsHandler(accUUID, time = "lifetime", game, interaction) {
   }
   const commands = await commandStorage.default.getCommands();
 
-  const res = await commands.gameStats.execute([accUUID, game, time], interaction.user.id, undefined, interaction);
+  const res = await commands.GameStats.execute([accUUID, game, time], interaction.user.id, undefined, interaction);
 
   return new ComponentResponse("", undefined, res.components, [res.file]);
 }
